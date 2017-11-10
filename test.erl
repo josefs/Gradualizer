609d766ddd79177511cf4f2bab3175faaec31939
Josef Svenningsson <josef.svenningsson@gmail.com>
2017-11-10 23:05:45 +0100

    More experiments with parsing erlang
    Now I'm able to get the specs from an erlang file.
    
    There's got to be a more robust way to parse erlang but the way I'm
    doing it now will suffice for the moment.

--- test.erl
+++ test.erl
@@ -2,19 +2,20 @@
 
 -compile([export_all]).
 
+-spec tokenize(string()) -> atom().
 tokenize(File) ->
     {ok, Binary} = file:read_file(File),
     case erl_scan:string(binary_to_list(Binary)) of
 	{error, ErrorInfo, ErrorLoc} ->
 	    io:format("Error: ~p~n~p~n",[ErrorInfo,ErrorLoc]);
-	{ok, Tokens, EndLoc} ->
-	    case erl_parse:parse_form(Tokens) of
-		{error, ErrorInfoP} ->
-		    io:format("Parse error: ~p~n", [ErrorInfoP]);
-		{ok, AbsForm} ->
-		    AbsForm
-	    end
+	{ok, Tokens, _EndLoc} ->
+	    Tokens
     end.
+
+parse(File) ->
+    Tokens = tokenize(File),
+    Toks = collect_forms(Tokens,[]),
+    Forms = lists:map(fun erl_parse:parse_form/1, Toks).
     %% case file:open(File,[read]) of
     %% 	{error, Reason} ->
     %% 	    io:format("File error: ~p~n",[Reason]);
@@ -27,3 +28,20 @@ tokenize(File) ->
     %% 	{error, ErrorInfo, ErrorLoc} ->
     %% 	    io:format("Error:  ~p~n~p~n",[ErrorInfo,ErrorLoc])
     %% end.
+
+collect_forms([],[]) -> [];
+collect_forms([],Acc) ->
+    [lists:reverse(Acc)];
+collect_forms([Tok={dot,_}|Tokens],Acc) ->
+    [lists:reverse([Tok|Acc])|collect_forms(Tokens,[])];
+collect_forms([Tok|Tokens],Acc) ->
+    collect_forms(Tokens,[Tok|Acc]).
+
+collect_specs([{attribute,_,spec,Spec}|Forms]) ->
+    [Spec|collect_specs(Forms)];
+collect_specs([_|Forms]) ->
+    collect_specs(Forms);
+collect_specs([]) -> [].
+
+parse_specs(File) ->
+    collect_specs(lists:map(fun ({ok,Form}) -> Form end, parse(File))).
