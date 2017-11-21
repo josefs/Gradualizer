4a617063bda7f96134a5396334886ba1f51f1bf1
Josef Svenningsson <josef.svenningsson@gmail.com>
2017-11-21 23:21:09 +0100

    Use epp for parsing. Initial type checking code.
    Thanks to David Wahlstedt for the suggestions to use epp.

--- test.erl
+++ test.erl
@@ -2,49 +2,16 @@
 
 -compile([export_all]).
 
--spec tokenize(string()) -> atom().
-tokenize(File) ->
-    {ok, Binary} = file:read_file(File),
-    case erl_scan:string(binary_to_list(Binary)) of
-	{error, ErrorInfo, ErrorLoc} ->
-	    io:format("Error: ~p~n~p~n",[ErrorInfo,ErrorLoc]);
-	{ok, Tokens, _EndLoc} ->
-	    Tokens
-    end.
-
-parse(File) ->
-    Tokens = tokenize(File),
-    Toks = collect_forms(Tokens,[]),
-    Forms = lists:map(fun erl_parse:parse_form/1, Toks).
-    %% case file:open(File,[read]) of
-    %% 	{error, Reason} ->
-    %% 	    io:format("File error: ~p~n",[Reason]);
-    %% 	{ok, Handle} ->
-    %% 	    file:read(Handle)
-    %% end.
-    %% case erl_scan:string(Str) of
-    %% 	{ok, Tokens, EndLoc} ->
-    %% 	    foo;
-    %% 	{error, ErrorInfo, ErrorLoc} ->
-    %% 	    io:format("Error:  ~p~n~p~n",[ErrorInfo,ErrorLoc])
-    %% end.
-
-collect_forms([],[]) -> [];
-collect_forms([],Acc) ->
-    [lists:reverse(Acc)];
-collect_forms([Tok={dot,_}|Tokens],Acc) ->
-    [lists:reverse([Tok|Acc])|collect_forms(Tokens,[])];
-collect_forms([Tok|Tokens],Acc) ->
-    collect_forms(Tokens,[Tok|Acc]).
-
+-spec collect_specs([any()]) -> [any()].
 collect_specs([{attribute,_,spec,Spec}|Forms]) ->
     [Spec|collect_specs(Forms)];
 collect_specs([_|Forms]) ->
     collect_specs(Forms);
 collect_specs([]) -> [].
 
-parse_specs(File) ->
-    collect_specs(lists:map(fun ({ok,Form}) -> Form end, parse(File))).
+get_specs(File) ->
+    {ok, Erl} = epp:parse_file(File,[]),
+    collect_specs(Erl).
 
 -type type() :: integer | float | boolean | {singleton, atom()}
 	      | {range, integer(), integer() }
@@ -70,3 +37,10 @@ parse_specs(File) ->
   % recover equality by adding type :: X as a constraint.
   % Perhaps we can argue for a new form of syntax.
 
+-spec type_check_expr(#{},any()) -> any().
+type_check_expr(Env, {var, _, Var}) ->
+    maps:get(Var, Env);
+type_check_expr(Env, {tuple, _, [TS]}) ->
+    [ type_check_expr(Env, Expr) || Expr <- TS ];
+type_check_expr(Env, {call, _, Name, Args}) ->
+    todo.
