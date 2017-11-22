21148c13f25040cfd48f35790f3dd0fe2756cbb9
Josef Svenningsson <josef.svenningsson@gmail.com>
2017-11-22 23:49:07 +0100

    Started seriously hacking on the typechecker
    My current goal is to have a functioning tool but for a very
    simple type system. Adding type system features can be done
    over time.

--- test.erl
+++ test.erl
@@ -37,10 +37,58 @@ get_specs(File) ->
   % recover equality by adding type :: X as a constraint.
   % Perhaps we can argue for a new form of syntax.
 
--spec type_check_expr(#{},any()) -> any().
-type_check_expr(Env, {var, _, Var}) ->
-    maps:get(Var, Env);
-type_check_expr(Env, {tuple, _, [TS]}) ->
-    [ type_check_expr(Env, Expr) || Expr <- TS ];
-type_check_expr(Env, {call, _, Name, Args}) ->
-    todo.
+-spec type_check_expr(#{},#{},any()) -> any().
+type_check_expr(_FEnv, VEnv, {var, _, Var}) ->
+    maps:get(Var, VEnv);
+type_check_expr(FEnv, VEnv, {tuple, _, [TS]}) ->
+    [ type_check_expr(FEnv, VEnv, Expr) || Expr <- TS ];
+type_check_expr(FEnv, VEnv, {call, _, Name, _Args}) ->
+    type_check_fun(FEnv, VEnv, Name).
+
+type_check_fun(FEnv, _VEnv, {atom, _, Name}) ->
+    maps:get(FEnv, Name);
+type_check_fun(FEnv, _VEnv, {remote, _, {atom,_,Module}, {atom,_,Fun}}) ->
+    maps:get(FEnv,{Module,Fun});
+type_check_fun(FEnv, VEnv, Expr) ->
+    type_check_expr(FEnv, VEnv, Expr).
+    
+
+type_check_clauses(_FEnv, _VEnv, _Clauses) ->
+    foo.
+
+type_check_function(FEnv, {function,_, Name, NArgs, Clauses}) ->
+    case maps:get(FEnv, Name) of
+	{ok, Type} -> apa;
+	error ->
+	    Types = type_check_clauses(FEnv, #{}, Clauses),
+	    merge_types(Types)
+    end.
+    
+type_check_file(File) ->
+    Forms = epp:parse_forms(File,[]),
+    {Specs, Funs} = collect_specs_and_functions(Forms),
+    FEnv = create_fenv(Specs),
+    lists:map(fun (Function) ->
+		      type_check_function(FEnv, Function) end, Funs).
+
+collect_specs_and_functions(Forms) ->
+    aux(Forms,[],[]).
+aux([], Specs, Funs) ->
+    {Specs, Funs};
+aux([Fun={function, _, _, _, _} | Forms], Specs, Funs) ->
+    aux(Forms, Specs, [Fun | Funs]);
+aux([{attribute, _, spec, Spec} | Forms], Specs, Funs) ->
+    aux(Forms, [Spec | Specs], Funs);
+aux([_|Forms], Specs, Funs) ->
+    aux(Forms, Specs, Funs).
+
+merge_types(apa) ->
+    error.
+
+create_fenv([{{Name,_},[Type]}|Specs]) ->
+    (create_fenv(Specs))#{Name => Type};
+create_fenv([{{Name,_},_}|_]) ->
+    throw({multiple_types_not_supported,Name});
+create_fenv([]) ->
+    #{}.
+
