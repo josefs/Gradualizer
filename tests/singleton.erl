55693b3329aa328ccb71cb59bef3a713f3fbb86f
Josef Svenningsson <josef.svenningsson@gmail.com>
2018-01-31 14:16:46 +0100

    Two new test modules for type declarations and singleton types
    Change-Id: I410890c0be8c23473a14bc3e54e8c8ea2a275eca

---
+++ tests/singleton.erl
@@ -0,0 +1,13 @@
+-module(singleton).
+
+-export([sing/1]).
+
+-spec sing(0|1) -> 10..12.
+sing(0) ->
+    10;
+sing(1) ->
+    12.
+
+-spec atom(a) -> b.
+atom(a) ->
+    b.
---
+++ tests/type_decl.erl
@@ -0,0 +1,5 @@
+-module(type_decl).
+
+-type a_type() :: bool().
+-opaque an_opaque_type() :: int().
+
