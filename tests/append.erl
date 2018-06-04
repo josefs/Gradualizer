2302e270c4c0822f360c77f6c6c2faac44b08df3
Josef Svenningsson <josef.svenningsson@gmail.com>
2018-06-04 08:28:26 +0200

    Update the append test

--- tests/append.erl
+++ tests/append.erl
@@ -2,7 +2,7 @@
 
 -compile(export_all).
 
-% Thanks to Kostis Sagonas for this example
+% Thanks to Kostis Sagonas for suggesting this example
 
 app1([], Ys) -> Ys;
 app1([X|Xs], Ys) -> [X|app1(Xs, Ys)].
@@ -14,3 +14,10 @@ app2([X|Xs], Ys) -> [X|app2(Xs, Ys)].
 -spec app3(list(), integer() | list()) -> integer() | list().
 app3([], Ys) -> Ys;
 app3([X|Xs], Ys) -> [X|app3(Xs, Ys)].
+
+-spec app4(list(A), list(A)) -> list(A).
+app4([], Ys) -> Ys;
+app4([X|Xs], Ys) -> [X|app4(Xs, Ys)].
+
+test() ->
+    app3([1,2,3], 4).
