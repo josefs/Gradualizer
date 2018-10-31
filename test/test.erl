ff9de6871b9176d67b87d7c141fe9f8e667ce7ab
Pierre Krafft <kpierre@outlook.com>
2018-10-31 21:38:19 +0100

    Add support for known problems

--- test/test.erl
+++ test/test.erl
@@ -6,16 +6,39 @@ should_pass_test_() ->
     %% user_types.erl references remote_types.erl
     %% it is not in the sourcemap of the DB so let's import it manually
     gradualizer_db:import_erl_files(["test/should_pass/user_types.erl"]),
-    run_tests_in("test/should_pass", ok).
+    map_erl_files(fun(File) ->
+            ?_assertEqual(ok, gradualizer:type_check_file(File))
+        end, "test/should_pass").
 
 should_fail_test_() ->
-    run_tests_in("test/should_fail", nok).
+    map_erl_files(fun(File) ->
+            ?_assertEqual(nok, gradualizer:type_check_file(File))
+        end, "test/should_fail").
 
-run_tests_in(Dir, ExpectedRes) ->
+% Test succeeds if Gradualizer crashes or if it doesn't type check.
+% Doing so makes the test suite notify us whenever a known problem is resolved.
+known_problem_should_pass_test_() ->
+    map_erl_files(fun(File) ->
+        Result =
+            try gradualizer:type_check_file(File) of
+                V -> V
+            catch E -> nok
+            end,
+        ?_assertEqual(nok, Result)
+        end, "test/known_problems/should_pass").
+
+% Test succeeds if Gradualizer crashes or if it does type check.
+% Doing so makes the test suite notify us whenever a known problem is resolved.
+known_problem_should_fail_test_() ->
+    map_erl_files(fun(File) ->
+        Result =
+            try gradualizer:type_check_file(File) of
+                V -> V
+            catch E -> ok
+            end,
+        ?_assertEqual(ok, Result)
+        end, "test/known_problems/should_fail").
+
+map_erl_files(Fun, Dir) ->
     Files = filelib:wildcard(filename:join(Dir, "*.erl")),
-
-    [{filename:basename(Dir) ++ ": " ++ filename:basename(File),
-      fun() ->
-              ?assert(ExpectedRes =:= gradualizer:type_check_file(File))
-      end}
-     || File <- Files].
+    lists:map(Fun, Files).
