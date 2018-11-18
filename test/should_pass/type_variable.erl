8a10b3473fdd13354e609e78b0f98a1fbd36d393
Ulf Norell <ulf.norell@gmail.com>
2018-11-18 22:39:22 +0100

    Added more complicated type variable test

--- test/should_pass/type_variable.erl
+++ test/should_pass/type_variable.erl
@@ -1,6 +1,19 @@
 -module(type_variable).
 
+-compile(export_all).
+
 -spec fff() -> Result when Result :: integer().
 fff() -> 0.
 
 ggg() -> -fff().
+
+%% Result should be pos_integer() in the end.
+-spec ff() -> Result when
+    Result       :: integer() | Intermediate,
+    Result       :: apple | Other,
+    Intermediate :: boolean() | Other,
+    Other        :: pos_integer().
+ff() -> 1.
+
+-spec gg() -> neg_integer().
+gg() -> -ff().
