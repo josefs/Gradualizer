e56e14839b34eef87fe8a715e234358886f219b3
Ulf Norell <ulf.norell@gmail.com>
2018-11-22 17:48:51 +0100

    Fix inferred types for logic ops

--- test/should_pass/shortcut_ops.erl
+++ test/should_pass/shortcut_ops.erl
@@ -38,3 +38,28 @@ orelse_infer2(B, N) -> B orelse N.
 -spec orelse_infer3(boolean(), boolean()) -> _.
 orelse_infer3(B, X) -> B orelse X.
 
+-spec is_false_number(false | number()) -> ok.
+is_false_number(_) -> ok.
+
+-spec is_true_number(true | number()) -> ok.
+is_true_number(_) -> ok.
+
+-spec is_bool(boolean()) -> ok.
+is_bool(_) -> ok.
+
+-spec check_inferred(boolean(), number()) -> _.
+check_inferred(B, N) ->
+    And1 = andalso_infer1(B, N),
+    ok   = is_false_number(And1),
+    And2 = andalso_infer2(B, N),
+    ok   = is_false_number(And2),
+    And3 = andalso_infer3(B, B),
+    ok   = is_bool(And3),
+    Or1  = orelse_infer1(B, N),
+    ok   = is_true_number(Or1),
+    Or2  = orelse_infer2(B, N),
+    ok   = is_true_number(Or2),
+    Or3  = orelse_infer3(B, B),
+    ok   = is_bool(Or3),
+    ok.
+
