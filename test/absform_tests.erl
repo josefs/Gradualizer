9deff70e3bb59d7e44556930eb57b94c649bde53
Ulf Norell <ulf.norell@gmail.com>
2018-11-22 13:31:50 +0100

    Unfold when-clauses also when checking `fun M:F/A` and `fun F/A` expressions

--- test/absform_tests.erl
+++ test/absform_tests.erl
@@ -7,14 +7,12 @@ function_type_list_to_fun_types_test() ->
         merl:quote("-spec f(T)-> boolean() when T :: tuple();"
                    "       (atom()) -> any()."),
     BoundedFunTypeList = absform:normalize_function_type_list(FunTypeList),
-    {Ty, Cs} = absform:function_type_list_to_fun_types(BoundedFunTypeList),
-    ?assertMatch({type, 1, union,
+    Ty = typechecker:bounded_type_list_to_type({tenv, [], []}, BoundedFunTypeList),
+    ?assertMatch({type, 0, union,
                   [{type,1,'fun',
-                    [{type,1,product,[{var,1,'T'}]},
+                    [{type,1,product,[{type,0,tuple,any}]},
                      {type,1,boolean,[]}]},
                    {type,1,'fun',
                     [{type,1,product,[{type,1,atom,[]}]},
                      {type,1,any,[]}]}]}, Ty),
-    ExpCs = constraints:upper('T', typelib:parse_type("tuple()")),
-    ?assertMatch(ExpCs, Cs),
     ok.
--- test/should_pass/type_variable.erl
+++ test/should_pass/type_variable.erl
@@ -7,6 +7,20 @@ fff() -> 0.
 
 ggg() -> -fff().
 
+%% Use string:words/1 since its type spec in OTP is
+%% -spec words(String) -> Count when
+%%       String :: string(),
+%%       Count :: pos_integer().
+
+ggg_ext() -> -string:words("a b c d").
+
+ggg_fun1() -> -(fun fff/0)().
+ggg_fun2() -> F = fun fff/0, -F().
+
+ggg_mfa() ->
+    F = fun string:words/1,
+    -F("a b c d").
+
 %% Result should be pos_integer() in the end.
 -spec ff() -> Result when
     Result       :: integer() | Intermediate,
