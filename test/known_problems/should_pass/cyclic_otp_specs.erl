b6c1dcbb5e8cc1f60ddd61e04f48b37182059b8b
Ulf Norell <ulf.norell@gmail.com>
2018-11-23 11:55:14 +0100

    Proper error for cyclic dependencies in type variables

---
+++ test/known_problems/should_pass/cyclic_otp_specs.erl
@@ -0,0 +1,9 @@
+-module(cyclic_otp_specs).
+
+-compile(export_all).
+
+%% We shouldn't fail on cyclic dependencies in OTP specs.
+%% Either make them work, or override the specs.
+
+-spec flatten([term()]) -> [term()].
+flatten(Xs) -> lists:flatten(Xs).
