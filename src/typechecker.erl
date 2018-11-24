84ebc4f2126f9ad53bc2df671b9314214c5408c3
Viktor Söderqvist <viktor@zuiderkwast.se>
2018-11-24 12:06:51 +0100

    Fix type error
    Dialyzer warning in the last build

--- src/typechecker.erl
+++ src/typechecker.erl
@@ -1007,7 +1007,7 @@ expect_tuple_union([], AccTy, AccCs, _NoAny, _N) ->
 %% types are returned with the original non-normalized type.
 -spec expect_fun_type(#env{}, type()) -> any
                                        | {type_error, type()}
-                                       | {fun_ty, type(), type(), constraints:constraints()}
+                                       | {fun_ty, [type()], type(), constraints:constraints()}
                                        | {fun_ty_any_args, type(), constraints:constraints()}
                                        | {fun_ty_intersection, [type()], constraints:constraints()}
                                        | {fun_ty_union, [any()], constraints:constraints()}
