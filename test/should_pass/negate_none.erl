d6cc78da90b71a05fa8acb07283ef99f5d6c4cbf
Ulf Norell <ulf.norell@gmail.com>
2018-11-26 09:38:15 +0100

    Don't crash if negating a none() value

---
+++ test/should_pass/negate_none.erl
@@ -0,0 +1,12 @@
+-module(negate_none).
+
+-compile([export_all, nowarn_export_all]).
+
+-spec crash() -> none().
+crash() -> error(crash).
+
+foo() -> -crash().
+
+-spec bar(none()) -> _.
+bar(X) -> -X.
+
