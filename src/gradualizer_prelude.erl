a31985684e15d76be0e1853c54f5f742483e6715
Péter Gömöri <gomoripeti@gmail.com>
2018-12-09 22:13:39 +0100

    Preludes: any() return from application:get_env/1/2/3

--- src/gradualizer_prelude.erl
+++ src/gradualizer_prelude.erl
@@ -4,3 +4,17 @@
 
 -spec erlang:hd([A, ...]) -> A.
 -spec erlang:tl([A, ...]) -> [A].
+
+%% change return Val to any() from term()
+-spec application:get_env(Par) -> 'undefined' | {'ok', Val} when
+      Par :: atom(),
+      Val :: any().
+-spec application:get_env(Application, Par) -> 'undefined' | {'ok', Val} when
+      Application :: atom(),
+      Par :: atom(),
+      Val :: any().
+-spec application:get_env(Application, Par, Def) -> Val when
+      Application :: atom(),
+      Par :: atom(),
+      Def :: term(),
+      Val :: any().
---
+++ test/should_pass/preludes.erl
@@ -0,0 +1,13 @@
+-module(preludes).
+
+-compile(export_all).
+
+%% use lists:sort (which only accepts list() not all term()) to test
+%% that V* is any()
+app_get_env() ->
+    {ok, V1} = application:get_env(param),
+    lists:sort(V1),
+    {ok, V2} = application:get_env(app, param),
+    lists:sort(V2),
+    V3 = application:get_env(app, param, default),
+    lists:sort(V3).
