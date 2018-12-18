8d88f302239b9983b5dfa184b6e95df2a0bf0a5c
Viktor Söderqvist <viktor@zuiderkwast.se>
2018-12-18 20:28:34 +0100

    Handle term() when expecting a list or a record type
    Also rename accidentally bound variable in comprehension checking
    causing crash when sefl-gradualizing.

--- test/should_pass/stuff_as_term.erl
+++ test/should_pass/stuff_as_term.erl
@@ -2,6 +2,8 @@
 
 -compile([export_all, nowarn_export_all]).
 
+-record(rec, {field :: term()}).
+
 -spec tuple_as_term() -> term().
 tuple_as_term() ->
     {x, y, z}.
@@ -9,3 +11,18 @@ tuple_as_term() ->
 -spec tuple_pat_as_term(term()) -> any().
 tuple_pat_as_term({X, Y}) ->
     {Y, X}.
+
+-spec cons_as_term() -> term().
+cons_as_term() -> [x, y].
+
+-spec cons_pat_as_term(term()) -> any().
+cons_pat_as_term([X|Xs]) -> X.
+
+-spec fun_as_term() -> term().
+fun_as_term() -> fun (X) -> X + 1 end.
+
+-spec record_as_term() -> term().
+record_as_term() -> #rec{}.
+
+-spec record_pat_as_term(term()) -> any().
+record_pat_as_term(#rec{}) -> ok.
