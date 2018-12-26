aecccc1a595313b491e64aacd153a47e97b48e8f
Viktor Söderqvist <viktor@zuiderkwast.se>
2018-12-26 01:05:05 +0100

    Test case for union with any()
    Test case where T | any() is used in a context T is not a valid type.
    
    The purpose is to make sure we don't normalize T | any() to just any().

---
+++ test/should_fail/union_with_any.erl
@@ -0,0 +1,24 @@
+-module(union_with_any).
+
+%% T | any() means
+%% "at least values of type T are possible; maybe also other values"
+
+-export([f1/1, f2/0]).
+
+%% With spec
+-spec f1(atom() | any()) -> any().
+f1(X) -> inc(X).
+
+%% Without spec
+f2() ->
+    AtomOrAny = receive
+                    1   -> get_atom();
+                    Any -> Any
+                end,
+    inc(AtomOrAny). %% Fails because atom() is possible
+
+-spec get_atom() -> atom().
+get_atom() -> banana.
+
+-spec inc(number()) -> number().
+inc(N) -> N + 1.
