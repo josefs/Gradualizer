68c043d332e4d6d8741ee086b2158c5127145f0f
Péter Gömöri <gomoripeti@gmail.com>
2018-12-28 15:30:27 +0100

    Known issue: Type refinement of list tails
    Issue #127

---
+++ test/known_problems/should_pass/refine_list_tail.erl
@@ -0,0 +1,9 @@
+-module(refine_list_tail).
+
+-compile([export_all]).
+
+%% Currently fails with:
+%% "The variable 'T' has type [] | [any(), ...] but is expected to have type [any(), ...]"
+-spec droplast(nonempty_list(T)) -> list(T).
+droplast([_T])  -> [];
+droplast([H|T]) -> [H|droplast(T)].
