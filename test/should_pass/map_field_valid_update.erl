f4007fa78533471dd9ed9d6440fadd5dbd8cd927
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2019-01-18 13:37:02 +0100

    Add tests for {map, _, _, _} type_error

---
+++ test/should_pass/map_field_valid_update.erl
@@ -0,0 +1,10 @@
+-module(map_field_valid_update).
+
+-export([f/1]).
+
+-type map_with_integer() :: #{a := integer()}.
+-type map_with_binary()  :: #{a := binary()}.
+
+-spec f(map_with_integer()) -> map_with_binary().
+f(#{} = Ctx) ->
+    Ctx#{a := <<"binary">>}.
