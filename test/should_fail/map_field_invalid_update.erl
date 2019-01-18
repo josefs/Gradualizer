216300ac7bf0abd88ee6b58710350fe4eddc3de2
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2019-01-18 14:26:13 +0100

    Get user defined types out of the equation

--- test/should_fail/map_field_invalid_update.erl
+++ test/should_fail/map_field_invalid_update.erl
@@ -2,9 +2,6 @@
 
 -export([f/1]).
 
--type map_with_integer() :: #{a := integer()}.
--type map_with_binary()  :: #{a := binary()}.
-
--spec f(map_with_integer()) -> map_with_binary().
+-spec f(#{a := integer()}) -> #{a := binary()}.
 f(#{} = Ctx) ->
     Ctx#{a := not_a_binary}.
