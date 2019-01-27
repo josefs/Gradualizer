0ad191404e10984316a90a4da6a74b2af3343bbd
Josef Svenningsson <josef.svenningsson@gmail.com>
2019-01-27 17:41:03 +0000

    New specs for functions in the lists module
    Some of the specs in the lists module in otp don't work so well with the gradualizer.
    This patch gives new specs which overrides the otp specs.
    
    Dialyzer crashes when given the gradualizer_prelude. It is probably
    confused by multiple specs for the same function. Therefore, we're
    hiding that module from the dialyzer.

--- src/gradualizer_prelude.erl
+++ src/gradualizer_prelude.erl
@@ -5,6 +5,7 @@
 -spec erlang:hd([A, ...]) -> A.
 -spec erlang:tl([A, ...]) -> [A].
 
+
 %% change return Val to any() from term()
 -spec application:get_env(Par) -> 'undefined' | {'ok', Val} when
       Par :: atom(),
@@ -18,3 +19,83 @@
       Par :: atom(),
       Def :: term(),
       Val :: any().
+
+-spec lists:append([[T]])     -> [T].
+-spec lists:append([T], [T])  -> [T].
+-spec lists:delete(T, [T])    -> [T].
+-spec lists:droplast([T, ...]) -> [T].
+
+-spec lists:dropwhile(fun((T) -> boolean()), [T]) -> [T].
+
+-spec lists:duplicate(T, non_neg_integer()) -> [T].
+
+-spec lists:filter(fun((T) -> boolean()), [T]) -> [T].
+-spec lists:filtermap(fun((Elem) -> boolean() | {'true', Value}), [Elem])
+		     -> [Elem | Value].
+
+-spec lists:flatmap(fun((A) -> [B]), [A]) -> [B].
+
+-type deep_list(A) :: [A | deep_list(A)].
+
+-spec lists:flatten(deep_list(A))      -> [A].
+-spec lists:flatten(deep_list(A), [A]) -> [A].
+
+-spec lists:foldl(fun((T, Acc) -> Acc), Acc, [T]) -> Acc.
+-spec lists:foldr(fun((T, Acc) -> Acc), Acc, [T]) -> Acc.
+-spec lists:join(T, [T]) -> [T].
+
+-spec lists:foreach(fun((T) -> term()), [T]) -> ok.
+
+-spec lists:last([T, ...]) -> T.
+
+-spec lists:map(fun((A) -> B), [A]) -> [B].
+-spec lists:mapfoldl(fun((A, Acc) -> {B, Acc}), Acc, [A]) -> {[B], Acc}.
+-spec lists:mapfoldr(fun((A, Acc) -> {B, Acc}), Acc, [A]) -> {[B], Acc}.
+
+-spec lists:max([T, ...]) -> T.
+-spec lists:min([T, ...]) -> T.
+
+-spec lists:merge([[T]]) -> [T].
+-spec lists:merge([X], [Y]) -> [X | Y].
+-spec lists:merge(fun((A, B) -> boolean()), [A], [B]) -> [A | B].
+-spec lists:merge3([X], [Y], [Z]) -> [X | Y | Z].
+
+-spec lists:nth    (pos_integer(), [T, ...]) -> T.
+-spec lists:nthtail(pos_integer(), [T, ...]) -> [T].
+
+-spec lists:partition(fun((T) -> boolean()), [T]) -> {[T], [T]}.
+
+-spec lists:reverse([T]) -> [T].
+
+-spec lists:reverse([T], [T]) -> [T].
+
+-spec lists:sort([T]) -> [T].
+-spec lists:sort(fun((T, T) -> boolean()), [T]) -> [T].
+
+-spec lists:split(non_neg_integer(), [T]) -> {[T], [T]}.
+
+-spec lists:splitwith(fun((T) -> boolean()), [T]) -> {[T], [T]}.
+
+-spec lists:sublist([T],                non_neg_integer()) -> [T].
+-spec lists:sublist([T], pos_integer(), non_neg_integer()) -> [T].
+
+-spec lists:subtract([T], [T]) -> [T].
+
+-spec lists:takewhile(fun((T) -> boolean()), [T]) -> [T].
+
+-spec lists:umerge([[T]]) -> [T].
+-spec lists:umerge([X], [Y]) -> [X | Y].
+-spec lists:umerge(fun((A, B) -> boolean()), [A], [B]) -> [A | B].
+-spec lists:umerge3([X], [Y], [Z]) -> [X | Y | Z].
+
+-spec lists:unzip ([{A, B}])    -> {[A], [B]}.
+-spec lists:unzip3([{A, B, C}]) -> {[A], [B], [C]}.
+
+-spec lists:usort([T]) -> [T].
+-spec lists:usort(fun((T, T) -> boolean()), [T]) -> [T].
+
+-spec lists:zip ([A], [B])      -> [{A, B}].
+-spec lists:zip3([A], [B], [C]) -> [{A, B, C}].
+-spec lists:zipwith (fun((X, Y)    -> T), [X], [Y])      -> [T].
+-spec lists:zipwith3(fun((X, Y, Z) -> T), [X], [Y], [Z]) -> [T].
+
