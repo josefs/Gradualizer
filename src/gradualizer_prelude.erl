a98405281347aca57132ee2443305fd98617172b
Josef Svenningsson <josefs@fb.com>
2019-02-28 09:19:46 +0000

    Alternative specs for mnesia

--- src/gradualizer_prelude.erl
+++ src/gradualizer_prelude.erl
@@ -218,3 +218,166 @@
 				  ,erl_eval:binding_struct()) ->
 					  {ok, erl_parse:abstract_expr()}
 					| {not_ok, {error, module(), any()}}.
+
+%% Mnesia
+
+-type t_result(Res) :: {'atomic', Res} | {'aborted', Reason::any()}.
+-type activity() :: 'ets' | 'async_dirty' | 'sync_dirty' | 'transaction' | 'sync_transaction' |
+                    {'transaction', Retries::non_neg_integer()} |
+                    {'sync_transaction', Retries::non_neg_integer()}.
+-type config_key() :: extra_db_nodes | dc_dump_limit.
+-type config_value() :: [node()] | number().
+-type config_result() :: {ok, config_value()} | {error, any()}.
+-type write_locks() :: 'write' | 'sticky_write'.
+-type read_locks() :: 'read'.
+-type lock_kind() :: write_locks() | read_locks().
+-type create_option() ::
+        {'access_mode', 'read_write' | 'read_only'} |
+        {'attributes', [atom()]} |
+        {'disc_copies', [node()]} |
+        {'disc_only_copies', [node]} |
+        {'index', [atom() | non_neg_integer()]} |
+        {'load_order', non_neg_integer()} |
+        {'majority', boolean()} |
+        {'ram_copies', [node()]} |
+        {'record_name', atom()} |
+        {'snmp', any()} |
+        {'storage_properties', [{Backend::module(), [BackendProp::_]}]} |
+        {'type', 'set' | 'ordered_set' | 'bag'} |
+        {'local_content', boolean()} |
+        {'user_properties', proplists:proplist()}.
+
+-spec mnesia:start() -> 'ok' | {'error', any()}.
+-spec mnesia:start([{Option::atom(), Value::_}]) -> 'ok' | {'error', any()}.
+-spec mnesia:stop() -> 'stopped' | {'error', any()}.
+-spec mnesia:change_config(Config::config_key(), Value::config_value()) ->
+				  config_result().
+-spec mnesia:transaction(Fun) -> t_result(Res) when
+      Fun :: fun(() -> Res).
+-spec mnesia:transaction(Fun, Retries) -> t_result(Res) when
+      Fun :: fun(() -> Res),
+      Retries :: non_neg_integer() | 'infinity';
+			(Fun, [Arg::_]) -> t_result(Res) when
+      Fun :: fun((...) -> Res).
+-spec mnedia:transaction(Fun, [Arg::_], Retries) -> t_result(Res) when
+      Fun :: fun((...) -> Res),
+      Retries :: non_neg_integer() | 'infinity'.
+-spec mnesia:sync_transaction(Fun) -> t_result(Res) when
+      Fun :: fun(() -> Res).
+-spec mnesia:sync_transaction(Fun, Retries) -> t_result(Res) when
+      Fun :: fun(() -> Res),
+      Retries :: non_neg_integer() | 'infinity';
+                      (Fun, [Arg::_]) -> t_result(Res) when
+      Fun :: fun((...) -> Res).
+-spec mnesia:sync_transaction(Fun, [Arg::_], Retries) -> t_result(Res) when
+      Fun :: fun((...) -> Res),
+      Retries :: non_neg_integer() | 'infinity'.
+-spec mnesia:activity(Kind, Fun) -> t_result(Res) | Res when
+      Kind :: activity(),
+      Fun  :: fun(() -> Res).
+-spec mnesia:activity(Kind, Fun, [Arg::_]) -> t_result(Res) | Res when
+      Kind :: activity(),
+      Fun  :: fun((...) -> Res);
+              (Kind, Fun, Mod) -> t_result(Res) | Res when
+      Kind :: activity(),
+      Fun  :: fun(() -> Res),
+      Mod  :: atom().
+-spec mnesia:activity(Kind, Fun, [Arg::_], Mod) -> t_result(Res) | Res when
+      Kind :: activity(),
+      Fun  :: fun((...) -> Res),
+      Mod  :: atom().
+-spec mnedia:first(atom()) -> any().
+-spec mnesia:last(atom()) -> any().
+-spec mnesia:next(atom(), any()) -> any().
+-spec mnesia:prev(atom(), any()) -> any().
+-spec mnesia:select(atom(), ets:match_spec()) -> [any()].
+-spec mnesia:select(atom(), ets:match_spec(), lock_kind()) -> [any()].
+-spec mnesia:select(any()) -> {[any()], any()} | '$end_of_table'.
+-spec mnesai:all_keys(atom()) -> [any()].
+-spec mnesia:dirty_select(atom(), ets:match_spec()) -> [any()].
+-spec mnesia:dirty_all_keys(atom()) -> [any()].
+-spec mnesia:dirty_first(atom()) -> any().
+-spec mnesia:dirty_last(atom()) -> any().
+-spec mnesia:dirty_next(atom(), any()) -> any().
+-spec mnesia:dirty_prev(atom(), any()) -> any().
+-spec mnesia:table_info(atom(), any()) -> any().
+-spec mnesia:get_backend_types() -> [any()].
+-spec mnesia:get_index_plugins() -> [any()].
+-spec mnesia:system_info(any()) -> any().
+-spec mnesia:create_schema([node()]) -> 'ok' | {'error', any()}.
+-spec mnesia:create_schema([node()], [Prop]) -> 'ok' | {'error', any()} when
+      Prop :: BackendType | IndexPlugin,
+      BackendType :: {backend_types, [{Name::atom(), Module::module()}]},
+      IndexPlugin :: {index_plugins, [{{Name::atom()}, Module::module(), Function::atom()}]}.
+-spec mneisa:delete_schema([node()]) -> 'ok' | {'error', any()}.
+-spec mneisa:add_backend_type(atom(), module()) -> t_result('ok').
+-spec mnesia:backup(any()) -> 'ok' | {'error', any()}.
+-spec mnesia:backup(any(), module()) -> 'ok' | {'error', any()}.
+-spec mnesia:traverse_backup(any(), any(), Fun, Acc) ->
+                             {'ok', Acc} | {'error', any()} when
+      Fun :: fun((Items, Acc) -> {Items,Acc}).
+-spec mnesia:traverse_backup(any(), module(),
+                      any(), module(),
+                      Fun, Acc) ->
+                             {'ok', Acc} | {'error', any()} when
+      Fun :: fun((Items, Acc) -> {Items,Acc}).
+-spec mnesia:install_fallback(any()) -> 'ok' | {'error', any()}.
+-spec mnesia:install_fallback(any(), module()|[Opt]) ->
+                              'ok' | {'error', any()} when
+      Opt :: Module | Scope | Dir,
+      Module :: {'module', Mod::module()},
+      Scope :: {'scope', 'global' | 'local'},
+      Dir :: {'mnesia_dir', Dir::string()}.
+-spec mnesia:uninstall_fallback() -> 'ok' | {'error', any()}.
+-spec mnesia:uninstall_fallback(Args) -> 'ok' | {'error', any()} when
+      Args :: [{'mnesia_dir', Dir::string()}].
+-spec mnesia:activate_checkpoint([Arg]) -> {'ok', Name, [node()]} | {'error', any()} when
+      Arg :: {'name', Name} | {'max', [atom()]} | {'min', [atom()]} |
+             {'allow_remote', boolean()} | {'ram_overrides_dump', boolean()}.
+-spec mnesia:deactivate_checkpoint(any()) -> 'ok' | {'error', any()}.
+-spec mnesia:backup_checkpoint(any(), any()) -> 'ok' | {'error', any()}.
+-spec mnesia:backup_checkpoint(any(), any(), module()) -> 'ok' | {'error', any()}.
+-spec mnesia:restore(any(), [Arg]) -> t_result([atom()]) when
+      Op  :: 'skip_tables' | 'clear_tables' | 'keep_tables' | 'restore_tables',
+      Arg :: {'module', module()} | {Op, [atom()]} | {'default_op', Op}.
+-spec mnesia:create_table([Arg]) -> t_result('ok') when
+      Arg :: {'name', atom()} | create_option().
+-spec mnesia:create_table(atom(), [create_option()]) -> t_result('ok').
+-spec mnesia:delete_table(atom()) -> t_result('ok').
+-spec mnesia:add_table_copy(atom(), node()
+			   ,'ram_copies' | 'disc_copies' | 'disc_only_copies')
+			   -> t_result(ok).
+-spec mnesia:del_table_copy(atom(), node()) -> t_result(ok).
+-spec mnesia:move_table_copy(atom(), node(), node()) -> t_result(ok).
+-spec mnesia:add_table_index(atom(), atom() | non_neg_integer()) -> t_result(ok).
+-spec mnesia:del_table_index(atom(), atom() | non_neg_integer()) -> t_result(ok).
+-spec mnesia:transform_table(atom(), Fun, [atom()]) -> t_result(ok) when
+      Fun:: fun((tuple()) -> tuple()) | ignore.
+-spec mnesia:transform_table(atom(), Fun, [atom()], atom()) -> t_result(ok) when
+      Fun:: fun((tuple()) -> tuple()) | ignore.
+-spec mnesia:change_table_copy_type(atom(), node()
+				   ,'ram_copies' | 'disc_copies' | 'disc_only_copies') -> t_result(ok).
+-spec mnesia:clear_table(atom()) -> t_result(ok).
+-spec mnesia:read_table_property(atom(), any()) -> tuple().
+-spec mnesia:write_table_property(atom(), tuple()) -> t_result(ok).
+-spec mnesia:delete_table_property(atom(), any()) -> t_result(ok).
+-spec mnesia:change_table_frag(atom(), any()) -> t_result(ok).
+-spec mnesia:dump_tables([atom()]) -> t_result(ok).
+-spec mnesia:wait_for_tables([atom()], timeout()) ->
+      'ok' | {'timeout', [atom()]} | {'error', any()}.
+-spec mnesia:force_load_table(atom()) -> 'yes' | {'error', any()}.
+-spec mnesia:change_table_access_mode(atom(), Mode) -> t_result(ok) when
+      Mode :: 'read_only'|'read_write'.
+-spec mnesia:change_table_load_order(atom(), Order) -> t_result(ok) when
+      Order :: non_neg_integer().
+-spec mnesia:change_table_majority(atom(), boolean()) -> t_result(ok).
+-spec mnesia:set_master_nodes([node()]) -> 'ok' | {'error', any()}.
+-spec mnesia:set_master_nodes(atom(), [node()]) ->
+                              'ok' | {'error', any()}.
+-spec mnesia:sync_log() -> 'ok' | {'error', any()}.
+-spec mnesia:subscribe(What) -> {'ok', node()} | {'error', any()} when
+      What :: 'system' | 'activity' | {'table', atom(), 'simple' | 'detailed'}.
+-spec mnesia:unsubscribe(What) -> {'ok', node()} | {'error', any()} when
+      What :: 'system' | 'activity' | {'table', atom(), 'simple' | 'detailed'}.
+-spec mnesia:load_textfile(file:filename()) -> t_result(ok) | {'error', any()}.
+-spec mnesia:dump_to_textfile(file:filename()) -> 'ok' | 'error' | {'error', any()}.
