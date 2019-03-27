afc1b1a6c745b40a3e277e343e362e02e5d5b09d
Péter Gömöri <gomoripeti@gmail.com>
2019-03-28 00:46:35 +0100

    known problem: checking function with overloaded spec

--- test/known_problems/should_pass/intersection.erl
+++ test/known_problems/should_pass/intersection.erl
@@ -1,5 +1,5 @@
 -module(intersection).
--export([f/1, g/1, j/1]).
+-export([f/1, g/1, j/1, num/1]).
 
 -spec f(a|b) -> a|b.
 f(V) -> h(V).
@@ -19,3 +19,11 @@ i(V, U) -> {V, U}.
 
 -spec j({a, b} | {d, e}) -> {a, b} | {d, e}.
 j({V, U}) -> i(V, U).
+
+%% Currently the function is checked against each part of the
+%% intersection spec one by one and fails with an error "The [second]
+%% clause cannot be reached" because 2 :: 1.
+-spec num(1) -> one;
+         (2) -> two.
+num(1) -> one;
+num(2) -> two.
