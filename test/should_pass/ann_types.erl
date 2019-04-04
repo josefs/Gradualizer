4088f35bc17a24fb01e2e30a0267b65a8f85fcdb
Péter Gömöri <gomoripeti@gmail.com>
2019-04-05 01:11:41 +0200

    Fix ann_type in map update

--- test/should_pass/ann_types.erl
+++ test/should_pass/ann_types.erl
@@ -6,3 +6,7 @@
 -spec f(A :: atom(), integer()) -> {A :: term(), B :: number()}.
 f(A, B) ->
     {A, B}.
+
+-spec g(A :: atom(), Map :: map()) -> NewMap :: map().
+g(A, Map) ->
+    Map#{ A => any }.
