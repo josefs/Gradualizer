62fe3b195ff2f19b6dc92f6c7a509da5475940f9
Viktor Söderqvist <viktor@zuiderkwast.se>
2019-09-21 22:09:49 +0200

    Add known problems for type refinement
    Related to issue #25

---
+++ test/known_problems/should_pass/refine_mismatch_using_guard_bifs.erl
@@ -0,0 +1,31 @@
+-module(refine_mismatch_using_guard_bifs).
+
+-export([refine_by_guards_1/1,
+         refine_by_guards_2/1,
+         refine_literals_by_guards/1]).
+
+-spec refine_by_guards_1(atom() | pos_integer() | float() |
+                         binary() | tuple() | [any()] |
+                         map() | pid() | reference() | port()) -> ok | port().
+refine_by_guards_1(X) when is_atom(X)      -> ok;
+refine_by_guards_1(X) when is_number(X)    -> ok;
+refine_by_guards_1(X) when is_bitstring(X) -> ok;
+refine_by_guards_1(X) when is_tuple(X)     -> ok;
+refine_by_guards_1(X) when is_list(X)      -> ok;
+refine_by_guards_1(X) when is_map(X)       -> ok;
+refine_by_guards_1(X) when is_pid(X)       -> ok;
+refine_by_guards_1(X) when is_reference(X) -> ok;
+refine_by_guards_1(X)                      -> X.
+
+-spec refine_by_guards_2(neg_integer() | 2..4 | boolean() | port() | ok) -> ok.
+refine_by_guards_2(X) when is_integer(X) -> ok;
+refine_by_guards_2(X) when is_boolean(X) -> ok;
+refine_by_guards_2(X) when is_port(X)    -> ok;
+refine_by_guards_2(X)                    -> X.
+
+-spec refine_literals_by_guards(banana | 0 | {} | [] | pid()) -> pid().
+refine_literals_by_guards(X) when is_atom(X)    -> self();
+refine_literals_by_guards(X) when is_integer(X) -> self();
+refine_literals_by_guards(X) when is_tuple(X)   -> self();
+refine_literals_by_guards(X) when is_list(X)    -> self();
+refine_literals_by_guards(X)                    -> X.
