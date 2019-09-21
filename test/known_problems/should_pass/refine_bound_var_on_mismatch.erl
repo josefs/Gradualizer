eb2c5f3d38b42e20c3823308e64eab94333217a1
Viktor Söderqvist <viktor@zuiderkwast.se>
2019-09-21 22:09:49 +0200

    Add known problems for type refinement
    Related to issue #25

---
+++ test/known_problems/should_pass/refine_bound_var_on_mismatch.erl
@@ -0,0 +1,31 @@
+-module(refine_bound_var_on_mismatch).
+
+%% Note: Here we're refining an already bound variable
+
+-export([refined_var_not_matching_itself/1,
+         refine_bound_var_by_pattern_mismatch/1,
+         refine_bound_var_by_guard_bifs/1]).
+
+%% Current error: Var is expected to have type y | z but has type x | y | z
+-spec refined_var_not_matching_itself(x | y | z) -> ok.
+refined_var_not_matching_itself(Var) ->
+    case Var of
+        x   -> ok;
+        Var -> ok
+    end.
+
+%% Current error: Var is expected to have type ok but it has type ok | nok
+-spec refine_bound_var_by_pattern_mismatch(ok | nok) -> ok.
+refine_bound_var_by_pattern_mismatch(Var) ->
+    case Var of
+        nok -> ok;
+        _   -> Var
+    end.
+
+-spec refine_bound_var_by_guard_bifs(ok | integer() | map()) -> ok.
+refine_bound_var_by_guard_bifs(X) ->
+    if
+        is_map(X)     -> ok;
+        is_integer(X) -> ok;
+        true          -> X
+    end.
