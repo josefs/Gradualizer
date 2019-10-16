1b6822b17453f8e2130e525fc178b74815235973
Nelson Vides <nelson.vides@erlang-solutions.com>
2019-10-16 10:16:47 +0200

    Added test for guard_unreachable_branch

--- test/should_fail/fun_guard_info.erl
+++ test/should_fail/fun_guard_info.erl
@@ -1,8 +1,17 @@
 -module(fun_guard_info).
--compile([export_all]).
+-export([
+         fun_wrong_arity/1,
+         guard_unreachable_branch/1
+        ]).
 
 -spec fun_wrong_arity(any()) -> boolean().
 fun_wrong_arity(Fun) when is_function(Fun, 2) ->
     Fun(0);
 fun_wrong_arity(_Fun) -> true.
 
+-spec guard_unreachable_branch(atom() | list()) -> list().
+guard_unreachable_branch(Name)
+  when is_atom(Name), is_list(Name) ->
+    erlang:atom_to_list(Name);
+guard_unreachable_branch(_Name) ->
+    [].
