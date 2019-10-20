f823d5963f24f0f5272b8ab281d994b6b661c6d7
Pierre Krafft <kpierre@outlook.com>
2019-10-20 23:40:42 +0200

    Add new logic operator tests

--- test/should_fail/logic_op.erl
+++ test/should_fail/logic_op.erl
@@ -1,8 +1,37 @@
 -module(logic_op).
--export([failand/1, failandalso/1]).
+-export([failand/1, failand2/1,
+         failandleft/2, failandleft2/2,
+         failandright/2, failandright2/2,
+         failandalso/1, failandalso2/1]).
 
 -spec failand(boolean()) -> tuple().
 failand(X) -> X and X.
 
+-spec failand2(boolean()) -> tuple().
+failand2(X) ->
+    O = X and X,
+    O.
+
+-spec failandleft(boolean(), integer()) -> boolean().
+failandleft(B, N) -> N and B.
+
+-spec failandleft2(boolean(), integer()) -> boolean().
+failandleft2(B, N) ->
+    O = N and B,
+    O.
+
+-spec failandright(boolean(), integer()) -> boolean().
+failandright(B, N) -> B and N.
+
+-spec failandright2(boolean(), integer()) -> boolean().
+failandright2(B, N) ->
+    O = B and N,
+    O.
+
 -spec failandalso(boolean()) -> tuple().
 failandalso(X) -> X andalso {}.
+
+-spec failandalso2(boolean()) -> tuple().
+failandalso2(X) ->
+    O = X andalso {},
+    O.
