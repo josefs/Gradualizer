6b435ae0dbba9e397ab15f65c95767d20292ec8a
Pierre Krafft <kpierre@outlook.com>
2019-10-27 23:29:10 +0100

    Add test for unexported remote type

--- priv/test/undefined_errors_helper.erl
+++ priv/test/undefined_errors_helper.erl
@@ -1,11 +1,15 @@
 -module(undefined_errors_helper).
--export([und_rec/0, und_ty/0]).
+-export([und_rec/0, und_ty/0, not_exp_ty/0]).
 -export_type([j/0]).
 
 -type j() :: undefined_type().
+-type not_exported() :: ok.
 
 -spec und_rec() -> #undefined_record{}.
 und_rec() -> ok.
 
 -spec und_ty() -> undefined_errors:undefined_type().
 und_ty() -> ok.
+
+-spec not_exp_ty() -> not_exported().
+not_exp_ty() -> ok.
