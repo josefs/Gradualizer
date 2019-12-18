75df418941dc112eb498366c98b7f67b93ae8afa
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2019-12-18 10:01:37 +0100

    Minimize the error case

--- test/known_problems/should_pass/update_map_type_error.erl
+++ test/known_problems/should_pass/update_map_type_error.erl
@@ -4,12 +4,6 @@
 
 -spec t(list()) -> map().
 t(Rows) ->
-    lists:foldl(fun({Id, Name, Member}, #{members := []} = List) ->
-                        List#{id := Id,
-                              name := Name,
-                              members := [binary_to_list(Member)]};
-                   ({_, _, Member}, #{members := Members} = List) ->
-                        List#{members := [binary_to_list(Member) | Members]}
-                end, #{id => undefined,
-                       name => undefined,
-                       members => []}, Rows).
+    lists:foldl(fun(Id, Map) ->
+                        Map#{id := Id}
+                end, #{id => undefined}, Rows).
