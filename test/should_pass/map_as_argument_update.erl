edcd13a4a66041023f5c340805b2d50fe74a7d5b
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2019-12-22 12:34:21 +0100

    Apply review comment
    Since the test passes, it shouldn't be called 'error' anymore.

--- test/should_pass/map_as_argument_update.erl
+++ test/should_pass/map_as_argument_update.erl
@@ -1,6 +1,8 @@
--module(update_map_type_error).
-
+-module(map_as_argument_update).
 -compile(export_all).
+
+%% See https://github.com/josefs/Gradualizer/pull/233 for the bug report,
+%% and https://github.com/josefs/Gradualizer/pull/237 for the fix.
 
 -spec t(list()) -> map().
 t(Rows) ->
