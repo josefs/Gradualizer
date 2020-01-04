0ac15fe0dfb37572d599973cf1e5cb2dc52db3e1
Francois Brodeur <francois.brodeur@adgear.com>
2020-01-04 14:45:48 -0500

    Don't use gradualizer:top() for guard tests

--- test/should_fail/guard_fail.erl
+++ test/should_fail/guard_fail.erl
@@ -2,7 +2,7 @@
 
 -compile(export_all).
 
--spec wrong_guard(gradualizer:top()) -> atom() | not_atom.
+-spec wrong_guard(integer() | atom()) -> atom() | not_atom.
 wrong_guard(A) when is_integer(A) -> A;
 wrong_guard(_) -> not_atom.
 
@@ -14,19 +14,19 @@ wrong_guard(_) -> not_atom.
     f
 }).
 
--spec wrong_record(gradualizer:top()) -> #r2{} | not_r.
+-spec wrong_record(#r1{} | #r2{}) -> #r2{} | not_r2.
 wrong_record(R) when is_record(R, r1) -> R;
-wrong_record(_) -> not_r.
+wrong_record(_) -> not_r2.
 
--spec wrong_union(gradualizer:top()) -> integer() | float() | not_number.
+-spec wrong_union(integer() | float() | atom()) -> integer() | float() | not_number.
 wrong_union(I) when is_integer(I) -> I;
 wrong_union(F) when is_atom(F) -> F;
 wrong_union(_) -> not_number.
 
--spec wrong_union2(gradualizer:top()) -> integer() | float() | not_number.
+-spec wrong_union2(integer() | float() | atom()) -> integer() | float() | not_number.
 wrong_union2(N) when is_integer(N) orelse is_atom(N) -> N;
 wrong_union2(_) -> not_number.
 
--spec wrong_andalso(gradualizer:top()) -> atom() | not_integer.
+-spec wrong_andalso(integer() | atom()) -> atom() | not_integer.
 wrong_andalso(N) when is_integer(N) andalso is_atom(N) -> N;
 wrong_andalso(_) -> not_integer.
--- test/should_pass/guard.erl
+++ test/should_pass/guard.erl
@@ -2,87 +2,95 @@
 
 -compile(export_all).
 
--spec atom(gradualizer:top()) -> atom() | not_atom.
+-spec atom(atom() | integer()) -> atom() | not_atom.
 atom(A) when is_atom(A) -> A;
 atom(_) -> not_atom.
 
--spec binary(gradualizer:top()) -> binary() | not_binary.
+-spec binary(binary() | atom()) -> binary() | not_binary.
 binary(B) when is_binary(B) -> B;
 binary(_) -> not_binary.
 
--spec bitstring(gradualizer:top()) -> bitstring() | not_bitstring.
+-spec bitstring(bitstring() | atom()) -> bitstring() | not_bitstring.
 bitstring(B) when is_bitstring(B) -> B;
 bitstring(_) -> not_bitstring.
 
--spec boolean(gradualizer:top()) -> boolean() | not_boolean.
+-spec boolean(boolean() | atom()) -> boolean() | not_boolean.
 boolean(B) when is_boolean(B) -> B;
 boolean(_) -> not_boolean.
 
--spec float(gradualizer:top()) -> float() | not_float.
+-spec float(float() | atom()) -> float() | not_float.
 float(F) when is_float(F) -> F;
 float(_) -> not_float.
 
--spec integer(gradualizer:top()) -> integer() | not_integer.
+-spec integer(integer() | atom()) -> integer() | not_integer.
 integer(I) when is_integer(I) -> I;
 integer(_) -> not_integer.
 
--spec number(gradualizer:top()) -> number() | not_number.
+-spec number(number() | atom()) -> number() | not_number.
 number(N) when is_number(N) -> N;
 number(_) -> not_number.
 
--spec list(gradualizer:top()) -> list() | not_list.
+-spec list(list() | atom()) -> list() | not_list.
 list(L) when is_list(L) -> L;
 list(_) -> not_list.
 
--spec map(gradualizer:top()) -> map() | not_map.
+-spec map(map() | atom()) -> map() | not_map.
 map(M) when is_map(M) -> M;
 map(_) -> not_map.
 
--spec pid(gradualizer:top()) -> pid() | not_pid.
+-spec pid(pid() | atom()) -> pid() | not_pid.
 pid(P) when is_pid(P) -> P;
 pid(_) -> not_pid.
 
--spec port(gradualizer:top()) -> port() | not_port.
+-spec port(port() | atom()) -> port() | not_port.
 port(P) when is_port(P) -> P;
 port(_) -> not_port.
 
--spec reference(gradualizer:top()) -> reference() | not_reference.
+-spec reference(reference() | atom()) -> reference() | not_reference.
 reference(R) when is_reference(R) -> R;
 reference(_) -> not_reference.
 
--spec tuple(gradualizer:top()) -> tuple() | not_tuple.
+-spec tuple(tuple() | atom()) -> tuple() | not_tuple.
 tuple(T) when is_tuple(T) -> T;
 tuple(_) -> not_tuple.
 
--spec function(gradualizer:top()) -> function() | not_function.
+-spec function(function() | atom()) -> function() | not_function.
 function(F) when is_function(F) -> F;
 function(_) -> not_function.
+
+-spec function2(function() | atom()) -> function() | not_function.
+function2(F) when is_function(F, 1) -> F;
+function2(_) -> not_function.
 
 -record(r, {
     f
 }).
 
--spec record(gradualizer:top()) -> #r{} | not_r.
+-spec record(#r{} | atom()) -> #r{} | not_r.
 record(R) when is_record(R, r) -> R;
 record(_) -> not_r.
 
--spec multiple(gradualizer:top()) -> integer() | float() | not_number.
+-spec record2(#r{} | atom()) -> #r{} | not_r.
+record2(R) when is_record(R, r, 1) -> R;
+record2(_) -> not_r.
+
+-spec multiple(integer() | float() | atom()) -> integer() | float() | not_number.
 multiple(I) when is_integer(I) -> I;
 multiple(F) when is_float(F) -> F;
 multiple(_) -> not_number.
 
--spec orelse1(gradualizer:top()) -> integer() | float() | not_number.
+-spec orelse1(integer() | float() | atom()) -> integer() | float() | not_number.
 orelse1(N) when is_integer(N) orelse is_float(N) -> N;
 orelse1(_) -> not_number.
 
--spec orelse2(gradualizer:top()) -> integer() | float() | not_number.
+-spec orelse2(integer() | float() | atom()) -> integer() | float() | not_number.
 orelse2(N) when is_integer(N); is_float(N) -> N;
 orelse2(_) -> not_number.
 
--spec andalso1(gradualizer:top()) -> integer() | not_integer.
+-spec andalso1(integer() | atom()) -> integer() | not_integer.
 andalso1(N) when is_integer(N) andalso is_number(N) -> N;
 andalso1(_) -> not_integer.
 
--spec andalso2(gradualizer:top()) -> integer() | not_integer.
+-spec andalso2(integer() | atom()) -> integer() | not_integer.
 andalso2(N) when is_integer(N), is_number(N) -> N;
 andalso2(_) -> not_integer.
