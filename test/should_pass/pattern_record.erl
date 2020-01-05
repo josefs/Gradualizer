fb6b540a99378874107e8ab9353b8d2c46ff8f57
Francois Brodeur <francois.brodeur@adgear.com>
2020-01-04 23:40:45 -0500

    Don't use term()

--- test/should_pass/pattern_record.erl
+++ test/should_pass/pattern_record.erl
@@ -4,22 +4,22 @@
     field :: integer()
 }).
 
--spec clause_match(term()) -> #test{} | not_test.
+-spec clause_match(#test{} | whatever) -> #test{} | not_test.
 clause_match(#test{} = Test) -> Test;
 clause_match(_) -> not_test.
 
--spec clause_match2(term()) -> #test{} | not_test.
+-spec clause_match2(#test{} | whatever) -> #test{} | not_test.
 clause_match2(Test = #test{}) -> Test;
 clause_match2(_) -> not_test.
 
--spec pat_match(term()) -> #test{} | not_test.
+-spec pat_match(#test{} | whatever) -> #test{} | not_test.
 pat_match(T) ->
     case T of
         #test{} = Test -> Test;
         _ -> not_test
     end.
 
--spec pat_match2(term()) -> #test{} | not_test.
+-spec pat_match2(#test{} | whatever) -> #test{} | not_test.
 pat_match2(T) ->
     case T of
         Test = #test{} -> Test;
