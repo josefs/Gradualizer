98d8f185da514f3bb1f13b3c195786a63ca00044
Francois Brodeur <francois.brodeur@adgear.com>
2020-01-05 21:01:13 -0500

    Add fail tests

---
+++ test/should_fail/pattern_record_fail.erl
@@ -0,0 +1,19 @@
+-module(pattern_record_fail).
+
+-compile(export_all).
+
+-record(r1, {
+    f
+}).
+
+-record(r2, {
+    f
+}).
+
+-spec bad_match1(#r1{} | other) -> #r1{} | not_r1.
+bad_match1(R = #r2{}) -> R;
+bad_match1(_) -> not_r1.
+
+-spec bad_match2(#r1{} | #r2{}) -> #r1{} | not_r1.
+bad_match2(R = #r2{}) -> R;
+bad_match2(_) -> not_r1.
