700dc243238a119b50e47c8ad9b5854eb465d815
Viktor Söderqvist <viktor@zuiderkwast.se>
2020-01-18 18:01:28 +0100

    Add negative tests for type refinement

--- test/should_fail/type_refinement.erl
+++ test/should_fail/type_refinement.erl
@@ -1,6 +1,8 @@
 -module(type_refinement).
 -export([guard_prevents_refinement/2, imprecision_prevents_refinement/2,
-         multi_pat_fail_1/2]).
+         multi_pat_fail_1/2,
+         guard_prevents_refinement2/1,
+         pattern_prevents_refinement/2]).
 
 -spec guard_prevents_refinement(1..2, boolean()) -> 2.
 guard_prevents_refinement(N, Guard) ->
@@ -16,3 +18,12 @@ imprecision_prevents_refinement(_, X) -> X.
 -spec multi_pat_fail_1(a|b, a|b) -> {b, b}.
 multi_pat_fail_1(a, a) -> {b, b};
 multi_pat_fail_1(A, B) -> {A, B}. %% Not only {b, b} here
+
+-spec guard_prevents_refinement2(timestamp()) -> ok.
+guard_prevents_refinement2(X) when is_integer(X), X rem 7 == 0 -> ok;
+guard_prevents_refinement2(infinity) -> ok. % can still be an integer
+
+-spec pattern_prevents_refinement(timestamp(), any()) -> atom().
+pattern_prevents_refinement(X, X)    when is_integer(X) -> ok;
+pattern_prevents_refinement(X, {_Y}) when is_integer(X) -> ok;
+pattern_prevents_refinement(Inf, _) -> Inf. % Inf can still be an integer
