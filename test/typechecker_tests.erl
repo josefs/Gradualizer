88c50d1b1db88a60fc5c9c7110b8fc7874bacc1a
Viktor Söderqvist <viktor@zuiderkwast.se>
2020-01-20 23:45:58 +0100

    Move integer types handling to separate module

--- test/typechecker_tests.erl
+++ test/typechecker_tests.erl
@@ -561,28 +561,6 @@ illegal_forms_test_() ->
                                    "f(1 + A) -> ok."]))
     ].
 
-int_range_diff_test_() ->
-    [
-     ?_assertEqual([{1, 2}, {6, 10}],
-                   typechecker:int_range_diff({1, 10}, {3, 5})),
-     ?_assertEqual([{neg_inf, -11}],
-                   typechecker:int_range_diff({neg_inf, -1}, {-10, pos_inf})),
-     ?_assertEqual([{neg_inf, -11}],
-                   typechecker:int_range_diff({neg_inf, pos_inf}, {-10, pos_inf})),
-     ?_assertEqual([{10, pos_inf}],
-                   typechecker:int_range_diff({neg_inf, pos_inf}, {neg_inf, 9})),
-     ?_assertEqual([{10, pos_inf}],
-                   typechecker:int_range_diff({0, pos_inf}, {neg_inf, 9})),
-     ?_assertEqual([],
-                   typechecker:int_range_diff({1, 10}, {neg_inf, pos_inf})),
-     ?_assertEqual([{1, 10}],
-                   typechecker:int_range_diff({1, 10}, {-10, -1})),
-     ?_assertEqual([{2, 2}],
-                   typechecker:int_range_diff({1, 2}, {1, 1})),
-     ?_assertEqual([{1, 1}],
-                   typechecker:int_range_diff({1, 2}, {2, 2}))
-    ].
-
 %%
 %% Helper functions
 %%
