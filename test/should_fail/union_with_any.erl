96f8ff18f606833ed3d003f5163baaa3cd8b2490
Viktor Söderqvist <viktor@zuiderkwast.se>
2020-01-21 17:57:09 +0100

    Move a test case

--- test/should_fail/union_with_any.erl
+++ test/should_fail/union_with_any.erl
@@ -3,7 +3,7 @@
 %% T | any() means
 %% "at least values of type T are possible; maybe also other values"
 
--export([f1/1, f2/0]).
+-export([f1/1, f2/0, f3/1]).
 
 %% With spec
 -spec f1(atom() | any()) -> any().
@@ -22,3 +22,10 @@ get_atom() -> banana.
 
 -spec inc(number()) -> number().
 inc(N) -> N + 1.
+
+-record(r, {i :: integer()}).
+
+%% The variable is expected to have type #r{} but it has type undefined | any()
+-spec f3(any() | undefined) -> integer().
+f3(R) ->
+    R#r.i.
