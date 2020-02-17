1e39173a875b276318f2174c75e2d1117b270f3f
Viktor Söderqvist <viktor@zuiderkwast.se>
2020-02-17 22:46:45 +0100

    Remove spec overrides not needed anymore

--- priv/prelude/erlang.specs.erl
+++ priv/prelude/erlang.specs.erl
@@ -2,64 +2,16 @@
 
 %% This module contains specs to replace incorrect or inexact specs in OTP.
 
--spec apply(function(), [any()]) -> any().
--spec apply(module(), atom(), [any()]) -> any().
--spec binary_to_term(binary()) -> any().
--spec binary_to_term(binary(), [safe | used]) -> any() | {any(), pos_integer()}.
--spec element(pos_integer(), tuple()) -> any().
--spec erase() -> [{any(), any()}].
--spec erase(any()) -> any() | undefined.
--spec fun_info(function()) -> [{Item, any()}] when
-      Item :: arity
-	    | env
-	    | index
-	    | name
-	    | module
-	    | new_index
-	    | new_uniq
-	    | pid
-	    | type
-	    | uniq.
--spec fun_info(function(), Item) -> {Item, any()} when
-      Item :: arity
-	    | env
-	    | index
-	    | name
-	    | module
-	    | new_index
-	    | new_uniq
-	    | pid
-	    | type
-	    | uniq.
--spec get() -> [{any(), any()}].
--spec get(any()) -> any() | undefined.
--spec get_keys() -> [any()].
--spec get_stacktrace() -> [{ module()
-				  , atom()
-				  , arity() | [any()]
-				  , [{file, string()}|{line, pos_integer()}]}].
+%% The commented-out specs are the original specs from OTP 21 (erts-10.2.4) with
+%% type variebles unfolded if they occur only once in the spec.
+
+%% -spec hd([term(), ...]) -> term().
 -spec hd([A, ...]) -> A.
+
+%% -spec min(term(), term()) -> term().
+%% -spec max(term(), term()) -> term().
 -spec max(A, B) -> A | B.
 -spec min(A, B) -> A | B.
--spec port_call(port() | atom(), integer(), any()) -> any().
-%% TODO: process_info
--spec put(any(), any()) -> any().
--spec raise(Class, Reason, Stacktrace) -> no_return() when
-      Class      :: error | exit | throw,
-      Reason     :: any(),
-      Stacktrace :: [{module(), atom(), arity() | [any()]} |
-                     {function(), [any()]}] |
-                    [{module(), atom(), arity() | [any()], [{atom(), any()}]} |
-                     {function(), [any()], [{atom(), any()}]}].
--spec send(pid() | port() | atom() | {atom(), node()}, any()) -> any().
--spec send(pid() | port() | atom() | {atom(), node()}, any()
-                 ,[nosuspend | noconnect])
-                -> ok | nosuspend | noconnect.
-%% TODO: system_info({allocator, atom()) -> [term()]
-%% TODO: system_info({allocator_sizes, atom()) -> [term()]
-%% TODO: system_info(os_monotonic_time_source) -> [{atom(), term()}].
-%% TODO: system_info(os_system_time_source) -> [{atom(), term()}].
-%% TODO: system_info(Item :: c_compiler_used) -> {atom(), term()}.
-%% TODO: system_info(Item :: check_io) -> [term()]
+
+%% -spec tl([term(), ...]) -> [term()].
 -spec tl([A, ...]) -> [A].
--spec tuple_to_list(tuple()) -> list().
--- priv/prelude/lists.specs.erl
+++ priv/prelude/lists.specs.erl
@@ -1,82 +1,47 @@
 -module(lists).
 
-%% This module contains specs to replace incorrect or inexact specs in OTP.
-
--spec append([[T]])     -> [T].
--spec append([T], [T])  -> [T].
--spec delete(T, [T])    -> [T].
--spec droplast([T, ...]) -> [T].
-
--spec dropwhile(fun((T) -> boolean()), [T]) -> [T].
-
--spec duplicate(non_neg_integer(), T) -> [T].
-
--spec filter(fun((T) -> boolean()), [T]) -> [T].
--spec filtermap(fun((Elem) -> boolean() | {'true', Value}), [Elem])
-		     -> [Elem | Value].
-
--spec flatmap(fun((A) -> [B]), [A]) -> [B].
+%% This module contains specs to replace incorrect or inexact specs in OTP. The
+%% commented-out specs are the original specs given in OTP 21 (stdlib-3.7.1)
 
 -type deep_list(A) :: [A | deep_list(A)].
 
+%% flatten/1,2 are defined using recursive constraints, which is something we
+%% don't handle yet. We use a recursive user-defined type deep_list/1 instead.
+
+%% -spec flatten(DeepList) -> List when
+%%       DeepList :: [term() | DeepList],
+%%       List :: [term()].
 -spec flatten(deep_list(A))      -> [A].
+
+%% -spec flatten(DeepList, Tail) -> List when
+%%       DeepList :: [term() | DeepList],
+%%       Tail :: [term()],
+%%       List :: [term()].
 -spec flatten(deep_list(A), [A]) -> [A].
 
+%% The original fold{l,r} and mapfold{l,r} functions don't require that Acc is
+%% the same type thoughout the procedure. We do.
+
+%% -spec foldl(Fun, Acc0, List) -> Acc1 when
+%%       Fun :: fun((Elem :: T, AccIn) -> AccOut),
+%%       Acc0 :: term(),
+%%       Acc1 :: term(),
+%%       AccIn :: term(),
+%%       AccOut :: term(),
+%%       List :: [T],
+%%       T :: term().
 -spec foldl(fun((T, Acc) -> Acc), Acc, [T]) -> Acc.
 -spec foldr(fun((T, Acc) -> Acc), Acc, [T]) -> Acc.
--spec join(T, [T]) -> [T].
 
--spec foreach(fun((T) -> term()), [T]) -> ok.
-
--spec last([T, ...]) -> T.
-
--spec map(fun((A) -> B), [A]) -> [B].
+%% -spec mapfoldl(Fun, Acc0, List1) -> {List2, Acc1} when
+%%       Fun :: fun((A, AccIn) -> {B, AccOut}),
+%%       Acc0 :: term(),
+%%       Acc1 :: term(),
+%%       AccIn :: term(),
+%%       AccOut :: term(),
+%%       List1 :: [A],
+%%       List2 :: [B],
+%%       A :: term(),
+%%       B :: term().
 -spec mapfoldl(fun((A, Acc) -> {B, Acc}), Acc, [A]) -> {[B], Acc}.
 -spec mapfoldr(fun((A, Acc) -> {B, Acc}), Acc, [A]) -> {[B], Acc}.
-
--spec max([T, ...]) -> T.
--spec min([T, ...]) -> T.
-
--spec merge([[T]]) -> [T].
--spec merge([X], [Y]) -> [X | Y].
--spec merge(fun((A, B) -> boolean()), [A], [B]) -> [A | B].
--spec merge3([X], [Y], [Z]) -> [X | Y | Z].
-
--spec nth    (pos_integer(), [T, ...]) -> T.
--spec nthtail(pos_integer(), [T, ...]) -> [T].
-
--spec partition(fun((T) -> boolean()), [T]) -> {[T], [T]}.
-
--spec reverse([T]) -> [T].
-
--spec reverse([T], [T]) -> [T].
-
--spec sort([T]) -> [T].
--spec sort(fun((T, T) -> boolean()), [T]) -> [T].
-
--spec split(non_neg_integer(), [T]) -> {[T], [T]}.
-
--spec splitwith(fun((T) -> boolean()), [T]) -> {[T], [T]}.
-
--spec sublist([T],                non_neg_integer()) -> [T].
--spec sublist([T], pos_integer(), non_neg_integer()) -> [T].
-
--spec subtract([T], [T]) -> [T].
-
--spec takewhile(fun((T) -> boolean()), [T]) -> [T].
-
--spec umerge([[T]]) -> [T].
--spec umerge([X], [Y]) -> [X | Y].
--spec umerge(fun((A, B) -> boolean()), [A], [B]) -> [A | B].
--spec umerge3([X], [Y], [Z]) -> [X | Y | Z].
-
--spec unzip ([{A, B}])    -> {[A], [B]}.
--spec unzip3([{A, B, C}]) -> {[A], [B], [C]}.
-
--spec usort([T]) -> [T].
--spec usort(fun((T, T) -> boolean()), [T]) -> [T].
-
--spec zip ([A], [B])      -> [{A, B}].
--spec zip3([A], [B], [C]) -> [{A, B, C}].
--spec zipwith (fun((X, Y)    -> T), [X], [Y])      -> [T].
--spec zipwith3(fun((X, Y, Z) -> T), [X], [Y], [Z]) -> [T].
