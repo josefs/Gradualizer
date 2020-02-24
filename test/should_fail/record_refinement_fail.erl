9c7264bbc4db44e246bb3d1684db42b91553032a
Francois Brodeur <francois.brodeur@adgear.com>
2020-02-24 11:21:27 -0500

    More tests

--- test/should_fail/record_refinement_fail.erl
+++ test/should_fail/record_refinement_fail.erl
@@ -13,6 +13,10 @@ one_field2(R, _) -> R#one_field.a;
 one_field2(_, I) -> I.
 
 -record(refined_field, {f :: integer() | undefined}).
--spec refined_field(#refined_field{}) -> #refined_field{f :: atom()}.
-refined_field(#refined_field{f = undefined}) -> #refined_field{f = 0};
+-spec refined_field(#refined_field{}) -> #refined_field{f :: integer()}.
 refined_field(R) -> R.
+
+-spec refined_field2(#refined_field{}) -> #refined_field{f :: atom()}.
+refined_field2(#refined_field{f = undefined}) -> #refined_field{f = 0};
+refined_field2(R) -> R.
+
