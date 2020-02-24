446738cbbd58b842485643fa6078b9111b514ad6
Francois Brodeur <francois.brodeur@adgear.com>
2020-02-24 09:40:28 -0500

    Add an extra fail test case

--- test/should_fail/record_refinement_fail.erl
+++ test/should_fail/record_refinement_fail.erl
@@ -7,3 +7,7 @@
 -spec one_field(#one_field{}, integer()) -> integer().
 one_field(#one_field{a = I}, _) -> I;
 one_field(_, I) -> I.
+
+-spec one_field2(#one_field{}, integer()) -> integer().
+one_field2(R, _) -> R#one_field.a;
+one_field2(_, I) -> I.
