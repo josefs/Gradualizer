bd239a4fc46d4a27d9dee636405009a9a0c9ad97
Francois Brodeur <francois.brodeur@adgear.com>
2020-03-03 10:49:43 -0500

    Totally legal since it's optional

--- test/should_fail/map_field_invalid_update.erl
+++ test/should_fail/map_field_invalid_update.erl
@@ -1,13 +1,8 @@
 -module(map_field_invalid_update).
 
--export([f/1, g/1]).
+-export([f/1]).
 
 -spec f(#{a := integer()}) -> #{a := binary()}.
 f(#{} = Ctx) ->
     Ctx#{a := not_a_binary}.
-
--spec g(#{}) -> #{a := any(), b => any()}.
-g(Ctx) ->
-    Ctx2 = Ctx#{a => 5},
-    Ctx2.
 
