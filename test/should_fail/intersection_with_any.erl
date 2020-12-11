bf290481fb788af9ab3a4d8932173470bd5bc577
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-12-11 14:20:35 +0000

    feedback addressed

--- test/should_fail/intersection_with_any.erl
+++ test/should_fail/intersection_with_any.erl
@@ -1,14 +1,7 @@
 -module(intersection_with_any).
 
--export([
-    intersection_using_constraints/1,
-    intersection_using_constraints2/1]).
+-export([intersection_using_constraints/1]).
 
-%% X :: integer() & any() by the constraints
--spec intersection_using_constraints(X) -> 5 when X :: integer(), X :: any().
+-spec intersection_using_constraints(X) -> atom() when X :: integer(), X :: any() .
 intersection_using_constraints(X) ->
-    X.
-
--spec intersection_using_constraints2(X) -> atom() when X :: integer(), X :: any() .
-intersection_using_constraints2(X) ->
     X.
