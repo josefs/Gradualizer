e60912b7c9c28e1a772ffe7a0a6afff875740551
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-12-11 13:35:41 +0000

    should_fail/intersection_with_any - one more example

--- test/should_fail/intersection_with_any.erl
+++ test/should_fail/intersection_with_any.erl
@@ -1,8 +1,14 @@
 -module(intersection_with_any).
 
--export([intersection_using_constraints/1]).
+-export([
+    intersection_using_constraints/1,
+    intersection_using_constraints2/1]).
 
 %% X :: integer() & any() by the constraints
 -spec intersection_using_constraints(X) -> 5 when X :: integer(), X :: any().
 intersection_using_constraints(X) ->
+    X.
+
+-spec intersection_using_constraints2(X) -> atom() when X :: integer(), X :: any() .
+intersection_using_constraints2(X) ->
     X.
