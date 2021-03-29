bfc524a72e128a2bf504c3b9d06ddad5bfd14751
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2021-03-29 09:22:53 +0100

    removing confusing (for now) comment

--- test/known_problems/should_pass/type_pattern.erl
+++ test/known_problems/should_pass/type_pattern.erl
@@ -5,11 +5,6 @@
 
 -type mychar() :: char().
 
-%% The user type `mychar()' inside a list is not normalized when
-%% `add_types_pats/4' is called, but postponed later within
-%% `subtype(string(), [mychar()], TEnv)'. There was a typo that
-%% specifically in case of a string pattern VEnv was passed instead of
-%% TEnv.
 -spec f([mychar()]) -> any().
 f("foo") ->
     ok.
