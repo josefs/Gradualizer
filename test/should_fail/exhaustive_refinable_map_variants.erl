43f2c9e86a3453af277b8d312179eb0e37d3fbd0
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-09-21 08:58:13 +0200

    Enable exhaustiveness checking over map variants
    Map variants are going to be checked for exhaustiveness only if map fields are refinable.
    This adds type_diff() over maps and makes sure map patterns are always exact.

---
+++ test/should_fail/exhaustive_refinable_map_variants.erl
@@ -0,0 +1,16 @@
+-module(exhaustive_refinable_map_variants).
+
+%% This extends cases from test/should_fail/exhaustive_user_type.erl to variants defined as maps.
+
+-export([map_variants/1]).
+
+-export_type([map_sum_t/0]).
+
+-type map_sum_t() :: #{field_one := integer()}
+                   | #{field_two := integer()}.
+
+-spec map_variants(map_sum_t()) -> ok.
+map_variants(T) ->
+    case T of
+        #{field_one := _} -> ok
+    end.
