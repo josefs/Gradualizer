c4dd49788c2f9b72f6f44283a5f3e6c0480885fb
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-09-30 19:00:17 +0200

    Split test/should_pass/underscore.erl into a passing and failing examples

---
+++ test/should_fail/underscore_no_catch_all.erl
@@ -0,0 +1,7 @@
+-module(underscore_no_catch_all).
+
+-compile([export_all, nowarn_export_all]).
+
+-spec underscore(_) -> _.
+underscore(1) ->
+    "apa".
--- test/should_pass/underscore_catch_all.erl
+++ test/should_pass/underscore_catch_all.erl
@@ -1,7 +1,9 @@
--module(underscore).
+-module(underscore_catch_all).
 
 -compile([export_all, nowarn_export_all]).
 
 -spec underscore(_) -> _.
 underscore(1) ->
-    "apa".
+    "apa";
+underscore(_) ->
+    "bepa".
