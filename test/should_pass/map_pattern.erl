6ed887d7c536fdd1c6430266a9d77a80e534f894
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-09-30 20:25:57 +0200

    Fix and annotate with TODOs test/should_pass/map_pattern.erl

--- test/should_pass/map_pattern.erl
+++ test/should_pass/map_pattern.erl
@@ -8,15 +8,30 @@
 f(#{bepa := Bepa}) ->
     Bepa.
 
--spec key_subtype(#{atom() => integer()}) -> integer().
+%% If refinable(_) -> true, then this is not valid.
+%-spec key_subtype(#{atom() => integer()}) -> integer().
+%% But this is:
+-spec key_subtype(#{banana := integer()}) -> integer().
 key_subtype(#{banana := N}) ->
     N.
+%% Or this is:
+%-spec key_subtype(#{banana => integer()}) -> integer().
+%key_subtype(#{}) -> DefaultReturnValue;
+%key_subtype(#{banana := N}) ->
+%    N.
+%% Generally speaking, we expect recursive functions on lists to cover the empty list case,
+%% so it makes sense to expect users specifying that their functions take a map with an optional
+%% field handle the empty map case.
 
 -spec map_union(#{a => atom()} | #{b := boolean()}) -> atom().
 map_union(#{a := A}) -> A;
-map_union(#{b := B}) -> B.
+map_union(#{b := B}) -> B;
+%% TODO: This is still order dependent due to some reason, so this clause cannot be the first one;
+%%       possibly something is wrong with type_diff on maps...
+map_union(#{}) -> defaultIfANotPresent.
 
--spec any_map(map()) -> ok.
+%-spec any_map(map()) -> ok.
+-spec any_map(#{apa := _}) -> ok.
 any_map(#{apa := _}) -> ok.
 
 -spec map_term(gradualizer:top()) -> any().
