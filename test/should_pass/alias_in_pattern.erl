3add1c0549dae00d72cc8156296b91bdb063c9f5
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-09-30 20:39:48 +0200

    Specs have to be more specific

--- test/should_pass/alias_in_pattern.erl
+++ test/should_pass/alias_in_pattern.erl
@@ -2,15 +2,21 @@
 
 -compile([export_all, nowarn_export_all]).
 
--spec foo(tuple() | integer()) -> tuple().
+%% Too vague:
+%-spec foo(tuple() | integer()) -> tuple().
+-spec foo({_, _} | 42) -> tuple().
 foo(Left = {_X, _Y})       -> Left;
 foo(42 = N)                -> {N+N}.
 
--spec bar(tuple() | integer()) -> tuple().
+%% Too vague:
+%-spec bar(tuple() | integer()) -> tuple().
+-spec bar({_,_,_} | 33) -> tuple().
 bar({_,_,_} = Right) -> Right;
 bar(N = 33)          -> {N+N}.
 
--spec baz(tuple() | integer()) -> tuple().
+%% Misleading - there's no integer() clause at all:
+%-spec baz(tuple() | integer()) -> tuple().
+-spec baz({{}, 0}) -> tuple().
 baz({Inside, _} = {{}, 0}) -> Inside.
 
 -spec issue32({atom1, atom2} | atom4) -> {atom1, atom2} | atom3.
