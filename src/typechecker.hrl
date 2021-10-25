f515a7099d238b0fd9e37326674b09b1c340bd7c
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-10-25 18:56:53 +0200

    Pass union_size_limit in env()
    Prior to this commit, the Env got passed around, but was not really used by normalize().
    This meant that even an invalid Env didn't trigger any bugs.
    Now that we're using the Env to get the union size limit, it started triggering bugs where the env
    was not correctly adjusted to env() from tenv().

--- src/typechecker.hrl
+++ src/typechecker.hrl
@@ -1,13 +1,15 @@
 -ifndef(__TYPECHECKER_HRL__).
 -define(__TYPECHECKER_HRL__, true).
 
--record(env, {fenv     = #{},
-              imported = #{}   :: #{{atom(), arity()} => module()},
-              venv     = #{},
-              tenv             :: gradualizer_lib:tenv(),
-              infer    = false :: boolean(),
-              verbose  = false :: boolean(),
-              exhaust  = true  :: boolean()
+-record(env, {fenv              = #{},
+              imported          = #{}   :: #{{atom(), arity()} => module()},
+              venv              = #{},
+              tenv                      :: gradualizer_lib:tenv(),
+              infer             = false :: boolean(),
+              verbose           = false :: boolean(),
+              exhaust           = true  :: boolean(),
+              %% Performance hack: Unions larger than this limit are replaced by any() in normalization.
+              union_size_limit          :: non_neg_integer()
              }).
 
 -endif. %% __TYPECHECKER_HRL__
