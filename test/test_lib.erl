dcfaf07f97eaf6be97e4c26607801b01d2b668ef
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-10-28 22:49:22 +0200

    Add docstring to test_lib:create_env/2

--- test/test_lib.erl
+++ test/test_lib.erl
@@ -6,8 +6,24 @@
 create_env(Opts) ->
     create_env("", Opts).
 
-create_env(String, Opts) ->
-    Forms = ensure_form_list(merl:quote(lists:flatten(String))),
+%% @doc Create a `typechecker:env()' from a string containing `-type ...'
+%% or `-record(...)' definitions.
+%%
+%% Usage:
+%%
+%% 1> test_lib:create_env("-record(r, {f}).", []).
+%% {env,#{},#{},#{},
+%%      #{module => undefined,
+%%        records =>
+%%            #{r =>
+%%                  [{typed_record_field,{record_field,1,
+%%                                                     {atom,1,f},
+%%                                                     {atom,1,undefined}},
+%%                                       {type,0,any,[]}}]},
+%%        types => #{}},
+%%      false,false,true}
+create_env(TypeEnvString, Opts) ->
+    Forms = ensure_form_list(merl:quote(lists:flatten(TypeEnvString))),
     ParseData = typechecker:collect_specs_types_opaques_and_functions(Forms),
     typechecker:create_env(ParseData, Opts).
 
