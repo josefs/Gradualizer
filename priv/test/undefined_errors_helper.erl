fa30aeb8fb5c934d3b7bb9ee94d75951f09120c2
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-02-11 18:33:30 +0100

    Enumerate undefined types for easier interpretation of results

--- priv/test/undefined_errors_helper.erl
+++ priv/test/undefined_errors_helper.erl
@@ -4,15 +4,15 @@
               expands_to_undefined_remote/0,
               expands_to_struct_with_undefined_remote/0]).
 
--type j() :: undefined_type().
+-type j() :: undefined_type1().
 -type not_exported() :: ok.
--type expands_to_undefined_remote() :: undefined_errors:undefined_type().
--type expands_to_struct_with_undefined_remote() :: {struct, undefined_errors:undefined_type()}.
+-type expands_to_undefined_remote() :: undefined_errors:undefined_type2().
+-type expands_to_struct_with_undefined_remote() :: {struct, undefined_errors:undefined_type3()}.
 
 -spec und_rec() -> #undefined_record{}.
 und_rec() -> ok.
 
--spec und_ty() -> undefined_errors:undefined_type().
+-spec und_ty() -> undefined_errors:undefined_type4().
 und_ty() -> ok.
 
 -spec not_exp_ty() -> not_exported().
