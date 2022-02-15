1250c875dd8c269ee7945d978a76635ebd01f38c
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-02-15 21:08:48 +0100

    Test more undef/not_exported error cases

--- test/misc/undefined_errors.erl
+++ test/misc/undefined_errors.erl
@@ -1,5 +1,9 @@
 -module(undefined_errors).
 -export([remote_type/0,
+         remote_remote_type/0,
+         remote_struct_with_remote_type/0,
+         remote_struct_with_local_type/0,
+         remote_struct_with_record/0,
          remote_call/0,
          remote_record/0,
          normalize_remote_type/0,
@@ -7,6 +11,18 @@
 
 -spec remote_type() -> undefined_errors_helper:j().
 remote_type() -> ok.
+
+-spec remote_remote_type() -> undefined_errors_helper:expands_to_undefined_remote().
+remote_remote_type() -> ok.
+
+-spec remote_struct_with_remote_type() -> undefined_errors_helper:expands_to_struct_with_undefined_remote().
+remote_struct_with_remote_type() -> {struct, ok}.
+
+-spec remote_struct_with_local_type() -> undefined_errors_helper:expands_to_struct_with_undefined_local().
+remote_struct_with_local_type() -> {struct, ok}.
+
+-spec remote_struct_with_record() -> undefined_errors_helper:expands_to_struct_with_undefined_record().
+remote_struct_with_record() -> {struct, ok}.
 
 -spec remote_call() -> ok.
 remote_call() -> undefined_errors_helper:undefined_call().
