eeda4db435eae7613d6897c110ab7b024b3a8c1e
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-06-01 22:57:55 +0200

    test

--- test/should_pass/nonexhaustive_record_pattern.erl
+++ test/should_pass/nonexhaustive_record_pattern.erl
@@ -1,9 +1,14 @@
 -module(nonexhaustive_record_pattern).
 
--export([g/2]).
+-export([f/2,
+         g/2]).
 
 -record(env, {}).
 -type env() :: #env{}.
+
+-spec f(_, env()) -> ok.
+f([], _Env) -> ok;
+f([_Ty1|_Tys], _Env) -> ok.
 
 -spec g(integer(), env()) -> ok.
 g(1, _Env) -> ok;
