0b6176a5e669e289419e65f85b2758e5c35ea56e
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-06-06 10:13:35 +0200

    Add some binary pattern matching tests

--- test/should_pass/binary_exhaustiveness_checking.erl
+++ test/should_pass/binary_exhaustiveness_checking.erl
@@ -1,5 +1,7 @@
 -module(binary_exhaustiveness_checking).
 
+-gradualizer([infer]).
+
 -export([f/1, g/1, h/1, k/1]).
 
 f(<<>>) -> ok;
@@ -15,3 +17,16 @@ h(<<>>) -> ok.
 
 -spec k(binary()) -> ok.
 k(<<_:4, _:4, _/binary>>) -> ok.
+
+%% This case should pass, since we should detect that <<V:8, _/binary>> is a complex pattern for
+%% which we cannot guarantee it exhausts the binary() type (see `is_bin_pat_exhaustive')
+%% and therefore not try to do exhaustiveness checking.
+-spec l(binary()) -> ok.
+l(B) ->
+    V = $A,
+    case B of
+        <<>> ->
+            ok;
+        <<V:8, _/binary>> ->
+            ok
+    end.
