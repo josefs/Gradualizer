944cd942019035ffc76a4224de7fac754c6a2873
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-08-11 21:53:02 +0200

    Add explanations and cross references

--- test/known_problems/should_pass/lc_cannot_glb_different_variants.erl
+++ test/known_problems/should_pass/lc_cannot_glb_different_variants.erl
@@ -1,5 +1,9 @@
 -module(lc_cannot_glb_different_variants).
 
+%% Compare with:
+%% - test/should_fail/lc_generator_should_fail.erl
+%% - test/should_pass/lc_generator_not_none.erl
+
 -export([g/2]).
 
 -type t() :: {a, d}
--- test/should_pass/lc_generator_not_none.erl
+++ test/should_pass/lc_generator_not_none.erl
@@ -1,5 +1,9 @@
 -module(lc_generator_not_none).
 
+%% Compare with:
+%% - test/known_problems/should_pass/lc_cannot_glb_different_variants.erl
+%% - test/should_fail/lc_generator_should_fail.erl
+
 -export([g/1]).
 
 -type t() :: {a, d}
