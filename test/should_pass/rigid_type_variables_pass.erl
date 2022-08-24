8e15a010f51d4568cafc081b6c6d2647c1cad359
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-08-24 19:24:19 +0200

    Fix the comment [skip ci]

--- test/should_pass/rigid_type_variables_pass.erl
+++ test/should_pass/rigid_type_variables_pass.erl
@@ -5,6 +5,5 @@
 -spec id(A) -> A.
 id(X) -> X.
 
-%% Caused a crash in int_type_to_range/1 on {var, _, 'A'}.
-%% It's now fixed by assuming that a type var means any integer whatsoever.
+%% Used to cause a crash in int_type_to_range/1 on {var, _, 'A'}.
 foo() -> -id(42).
