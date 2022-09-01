cd65e7ca0a5b2c47dfc3788371e231d355ac10be
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-09-01 23:50:50 +0200

    Add another polymorphic function test

--- test/should_fail/poly_fail.erl
+++ test/should_fail/poly_fail.erl
@@ -2,8 +2,19 @@
 
 %% See "Bidirectional Typing for Erlang", N. Rajendrakumar, A. Bieniusa, 2021, Section 2. Examples.
 
--export([poly_2/1,
+-export([find1/0,
+         poly_2/1,
          poly_fail/3]).
+
+-spec lookup(T1, [{T1, T2}]) -> (none | T2).
+lookup(_, []) -> none;
+lookup(K, [{K, V}|_]) -> V;
+lookup(K, [_|KVs])-> lookup(K, KVs).
+
+-spec find1() -> string().
+find1() ->
+    %% This should fail type checking.
+    "s" = lookup(0, [{0, 1}]).
 
 -spec poly_2(fun((A) -> A)) -> {integer(), boolean()}.
 poly_2(F) -> {F(42), F(false)}.
