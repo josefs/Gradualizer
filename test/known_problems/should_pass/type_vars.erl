9544819636bc7634bbef21da98ad5cb92e43ac13
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-09-03 22:38:13 +0200

    Add a type var / union / case expr test
    Test that a type var is solved to a union type given there's a case expression with multiple
    patterns matching on an actual var the type var is assigned to.

--- test/known_problems/should_pass/type_vars.erl
+++ test/known_problems/should_pass/type_vars.erl
@@ -1,6 +1,9 @@
 -module(type_vars).
 
--export([foo/1]).
+-export([foo/1,
+         map_type_var/1]).
+
+-gradualizer([solve_constraints]).
 
 -spec foo([{integer(), integer()}]) -> [{integer(), integer()}].
 foo(Pairs) ->
@@ -8,3 +11,15 @@ foo(Pairs) ->
 
 -spec pair_sort([A]) -> [A] when A :: {gradualizer:top(), gradualizer:top()}.
 pair_sort(Ps) -> Ps.
+
+-spec map_type_var(nonempty_list(#{atom() => integer()} | atom())) -> integer().
+map_type_var(L) ->
+    V = lists:nth(2, L),
+    %% at this point V :: T
+    case V of
+        %% pattern matching a map against a type var
+        #{k := Int} ->
+            Int;
+        _ ->
+            0
+    end.
