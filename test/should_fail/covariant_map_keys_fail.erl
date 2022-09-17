37a9b1601d9a9995165ab3792c304198b963c3dd
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-09-17 19:09:15 +0200

    Extend test/should_fail/covariant_map_keys_fail.erl
    See https://github.com/josefs/Gradualizer/issues/432 for some context.

--- test/should_fail/covariant_map_keys_fail.erl
+++ test/should_fail/covariant_map_keys_fail.erl
@@ -3,11 +3,19 @@
 -compile([nowarn_unused_function]).
 -export([not_good/1]).
 
--spec good(#{ good := A }) -> A.
-good(#{ good := X }) -> X.
+-gradualizer([infer]).
+
+-spec good(#{good := A}) -> A.
+good(#{good := X}) -> X.
 
 -spec not_good(#{good | bad := A}) -> A.
 not_good(M) -> good(M). %% This call should fail
 
--spec kaboom() -> integer().
-kaboom() -> not_good(#{ bad => 0 }).
+-spec kaboom1() -> integer().
+kaboom1() ->
+    M = #{bad => 0},
+    not_good(M).
+
+-spec kaboom2() -> integer().
+kaboom2() ->
+    not_good(#{bad => 0}).
