8fe96910fee7b1401508739715205e0cb003cbf2
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-09-19 17:46:54 +0200

    Extract test/should_pass/map_infer_pass.erl from test/should_fail/covariant_map_keys_fail.erl

--- test/should_fail/covariant_map_keys_fail.erl
+++ test/should_fail/covariant_map_keys_fail.erl
@@ -1,21 +1,9 @@
 -module(covariant_map_keys_fail).
 
--compile([nowarn_unused_function]).
 -export([not_good/1]).
-
--gradualizer([infer]).
 
 -spec good(#{good := A}) -> A.
 good(#{good := X}) -> X.
 
 -spec not_good(#{good | bad := A}) -> A.
 not_good(M) -> good(M). %% This call should fail
-
--spec kaboom1() -> integer().
-kaboom1() ->
-    M = #{bad => 0},
-    not_good(M).
-
--spec kaboom2() -> integer().
-kaboom2() ->
-    not_good(#{bad => 0}).
---
+++ test/should_pass/map_infer_pass.erl
@@ -0,0 +1,19 @@
+-module(map_infer_pass).
+
+-export([not_good/1,
+         kaboom1/0,
+         kaboom2/0]).
+
+-gradualizer([infer]).
+
+-spec not_good(#{good | bad := integer()}) -> integer().
+not_good(#{good := _}) -> 0.
+
+-spec kaboom1() -> integer().
+kaboom1() ->
+    M = #{bad => 0},
+    not_good(M).
+
+-spec kaboom2() -> integer().
+kaboom2() ->
+    not_good(#{bad => 0}).
