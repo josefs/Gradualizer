ce35e8cc4cb2171281c3f9e147a28c149b4c6cd5
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-09-22 22:24:47 +0200

    Add test/should_pass/any_doesnt_have_type_none_should_pass.erl

---
+++ test/should_pass/any_doesnt_have_type_none_should_pass.erl
@@ -0,0 +1,17 @@
+-module(any_doesnt_have_type_none_should_pass).
+
+-type type() :: abstract_type().
+
+-type abstract_type() :: af_tuple_type().
+
+-type anno() :: erl_anno:anno().
+
+-type af_tuple_type() :: {'type', anno(), 'tuple', 'any'}
+                       | {'type', anno(), 'tuple', [abstract_type()]}.
+
+-spec f(type(), type()) -> ok.
+f(Ty1 = {type, _, tuple, Tys1}, Ty2 = {type, _, tuple, Tys2}) ->
+    case {Tys1, Tys2} of
+        {any, _} -> ok;
+        _ -> ok
+    end.
