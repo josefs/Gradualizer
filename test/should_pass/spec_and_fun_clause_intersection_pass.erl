927c7bc6c990ffe1b059625e8fdcd42ec91b7aac
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-09-24 19:07:16 +0200

    Move test/known_problems/should_pass/spec_and_fun_clause_intersection_should_pass.erl to should_pass

--- test/should_pass/spec_and_fun_clause_intersection_pass.erl
+++ test/should_pass/spec_and_fun_clause_intersection_pass.erl
@@ -1,9 +1,16 @@
 -module(spec_and_fun_clause_intersection_pass).
 
--export([foo/1]).
+-export([foo/1, g/1]).
 
 -spec foo(bar) -> bar;
          (list()) -> baz | qux.
 foo(bar) -> bar;
 foo([]) -> baz;
 foo([_|_]) -> qux.
+
+-type t() :: {tag, any()}.
+
+-spec g(t()) -> string();
+       ([t()]) -> string().
+g(Types) when is_list(Types) -> lists:map(fun g/1, Types);
+g({tag, _}) -> "".
