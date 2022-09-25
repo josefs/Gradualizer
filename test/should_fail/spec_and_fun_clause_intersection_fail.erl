545a63d6c1c14bd6b2ae62c58b4c5f6232722c2d
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-09-25 14:09:03 +0200

    Extend test/should_fail/spec_and_fun_clause_intersection_fail.erl with exhaustiveness checking test

--- test/should_fail/spec_and_fun_clause_intersection_fail.erl
+++ test/should_fail/spec_and_fun_clause_intersection_fail.erl
@@ -1,15 +1,21 @@
 -module(spec_and_fun_clause_intersection_fail).
 
--export([g/1]).
+-export([f/1, g/1]).
 
--type t() :: {tag, any()}.
+-type t() :: {tag, integer()}.
+
 
 %% Order of spec clauses and function clauses does not match - should fail!
 %% Strictly speaking, this does not follow the official Erlang docs to the letter,
 %% but supporting out of order matching of function clauses vs spec clauses
 %% is an implementation nightmare.
--spec g(t()) -> string();
+-spec f(t()) -> string();
        ([t()]) -> string().
-g([] = _Types) -> [];
-g([_|_] = Types) -> lists:map(fun g/1, Types);
+f([] = _Types) -> [];
+f([_|_] = Types) -> lists:map(fun f/1, Types);
+f({tag, _}) -> "".
+
+-spec g([t()]) -> string();
+       (t()) -> string().
+g([] = _Types) -> "";
 g({tag, _}) -> "".
