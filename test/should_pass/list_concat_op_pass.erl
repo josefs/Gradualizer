55d58dae70c32b7c83ccf6677879176ff39216bc
Łukasz Pauszek <lukasz.pauszek@erlang-solutions.com>
2022-11-17 14:42:06 +0100

    Expand erlang.++ function specs - add test

--- test/should_pass/list_concat_op_pass.erl
+++ test/should_pass/list_concat_op_pass.erl
@@ -4,7 +4,8 @@
          nonempty1_concat_fun_elem_gives_improper/0,
          nonempty2_concat_fun_elem_gives_improper/0,
          nil_concat_fun_nonempty_gives_proper/0,
-         nonempty_concat_fun_nonempty_gives_proper/0]).
+         nonempty_concat_fun_nonempty_gives_proper/0,
+         nonempty1_concat_fun_nonempty2_gives_proper/0]).
 
 -spec nil_concat_fun_elem_gives_elem() -> b.
 nil_concat_fun_elem_gives_elem() ->
@@ -25,3 +26,9 @@ nil_concat_fun_nonempty_gives_proper() ->
 -spec nonempty_concat_fun_nonempty_gives_proper() -> [atom()].
 nonempty_concat_fun_nonempty_gives_proper() ->
     erlang:'++'([a,b], [c]).
+
+-spec nonempty1_concat_fun_nonempty2_gives_proper() -> [integer()].
+nonempty1_concat_fun_nonempty2_gives_proper() ->
+    erlang:'++'(generate_list(), generate_list()).
+
+generate_list() -> lists:seq(1, 2).
