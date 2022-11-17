09aea0fe473fe70001cafd1bbf1d85b899af61e5
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-11-17 12:42:44 +0100

    Split test/should_pass/list_concat_op_pass.erl from test/known_problems/should_pass/list_op_should_pass.erl

--- test/known_problems/should_pass/list_concat_op_should_pass.erl
+++ test/known_problems/should_pass/list_concat_op_should_pass.erl
@@ -1,39 +1,22 @@
--module(list_op_should_pass).
+-module(list_concat_op_should_pass).
 
 -export([nil_concat_op_elem_gives_elem/0,
-         nil_concat_fun_elem_gives_elem/0,
          nonempty1_concat_op_elem_gives_improper/0,
-         nonempty1_concat_fun_elem_gives_improper/0,
          nonempty2_concat_op_elem_gives_improper/0,
-         nonempty2_concat_fun_elem_gives_improper/0,
          nil_concat_op_nonempty_gives_proper/0,
-         nil_concat_fun_nonempty_gives_proper/0,
-         nonempty_concat_op_nonempty_gives_proper/0,
-         nonempty_concat_fun_nonempty_gives_proper/0]).
+         nonempty_concat_op_nonempty_gives_proper/0]).
 
 -spec nil_concat_op_elem_gives_elem() -> b.
 nil_concat_op_elem_gives_elem() ->
     [] ++ b.
 
--spec nil_concat_fun_elem_gives_elem() -> b.
-nil_concat_fun_elem_gives_elem() ->
-    erlang:'++'([], b).
-
 -spec nonempty1_concat_op_elem_gives_improper() -> nonempty_improper_list(a, b).
 nonempty1_concat_op_elem_gives_improper() ->
     [a] ++ b.
 
--spec nonempty1_concat_fun_elem_gives_improper() -> nonempty_improper_list(a, b).
-nonempty1_concat_fun_elem_gives_improper() ->
-    erlang:'++'([a], b).
-
 -spec nonempty2_concat_op_elem_gives_improper() -> nonempty_improper_list(atom(), c).
 nonempty2_concat_op_elem_gives_improper() ->
     [a,b] ++ c.
-
--spec nonempty2_concat_fun_elem_gives_improper() -> nonempty_improper_list(atom(), c).
-nonempty2_concat_fun_elem_gives_improper() ->
-    erlang:'++'([a,b], c).
 
 %% See list_op_should_fail.
 %% -spec improper_concat_op_elem_gives_badarg() -> none().
@@ -44,17 +27,9 @@ nonempty2_concat_fun_elem_gives_improper() ->
 nil_concat_op_nonempty_gives_proper() ->
     [] ++ [a].
 
--spec nil_concat_fun_nonempty_gives_proper() -> [atom()].
-nil_concat_fun_nonempty_gives_proper() ->
-    erlang:'++'([], [a]).
-
 -spec nonempty_concat_op_nonempty_gives_proper() -> [atom()].
 nonempty_concat_op_nonempty_gives_proper() ->
     [a,b] ++ [c].
-
--spec nonempty_concat_fun_nonempty_gives_proper() -> [atom()].
-nonempty_concat_fun_nonempty_gives_proper() ->
-    erlang:'++'([a,b], [c]).
 
 %% See list_op_should_fail.
 %% -spec improper_concat_op_nonempty_gives_badarg() -> none().
