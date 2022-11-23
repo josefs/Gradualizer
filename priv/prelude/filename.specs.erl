2f493ac4eb7002de0a1387f866a5ddc984ce39ae
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-11-23 21:53:54 +0100

    Add filename:basename/1 spec override

--- priv/prelude/filename.specs.erl
+++ priv/prelude/filename.specs.erl
@@ -2,5 +2,8 @@
 
 -type deep_list() :: lists:deep_list(char() | atom()).
 
+-spec basename(binary()) -> binary();
+              (string() | atom() | deep_list()) -> string().
+
 -spec rootname(binary()) -> binary();
               (string() | atom() | deep_list()) -> string().
