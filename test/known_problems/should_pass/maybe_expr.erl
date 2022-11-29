35454a84bebfb8aadd58fd51aaad46e370915566
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-11-29 18:57:33 +0100

    Gate the maybe expression test to avoid crashing on older OTP versions

--- test/known_problems/should_pass/maybe_expr.erl
+++ test/known_problems/should_pass/maybe_expr.erl
@@ -1,7 +1,16 @@
 -module(maybe_expr).
+
+-ifdef(OTP_RELEASE).
+-if(?OTP_RELEASE >= 25).
+-if(?FEATURE_ENABLED(maybe_expr)).
+
 -export([syntax/0]).
 
 syntax() ->
     maybe
         ok ?= ok
     end.
+
+-endif. %% FEATURE_ENABLED
+-endif. %% OTP >= 25
+-endif. %% OTP_RELEASE
