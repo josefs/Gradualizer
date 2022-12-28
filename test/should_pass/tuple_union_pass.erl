f1ea01981c174a4a44de86a6b10c86279730c931
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-12-28 15:16:54 +0100

    Add a more complex union type to test/should_pass/tuple_union_pass.erl
    This raises hits on line 1245 of Cover output from 0 to 1:
    
    1233		-spec expect_tuple_union(Tys, [Tys], constraints:t(), any | no_any, non_neg_integer(), env()) -> R when
    1234		      Tys :: [type()],
    1235		      R :: {[Tys], constraints:t()}.
    1236		expect_tuple_union([Ty|Tys], AccTy, AccCs, Any, N, Env) ->
    1237	27	    case expect_tuple_type(Ty, N, Env) of
    1238		        {type_error, _} ->
    1239	9	            expect_tuple_union(Tys, AccTy, AccCs, Any, N, Env);
    1240		        any ->
    1241	1	            expect_tuple_union(Tys, AccTy, AccCs, any, N, Env);
    1242		        {elem_ty, TTy, Cs} ->
    1243	16	            expect_tuple_union(Tys, [TTy | AccTy], constraints:combine(Cs, AccCs), Any, N, Env);
    1244		        {elem_tys, TTys, Cs} ->
    1245	1	            expect_tuple_union(Tys, TTys ++ AccTy, constraints:combine(Cs, AccCs), Any, N, Env)
    1246		    end;
    1247		expect_tuple_union([], AccTy, AccCs, any, N, _Env) ->
    1248	1	    {[ lists:duplicate(N, type(any)) | AccTy], AccCs};
    1249		expect_tuple_union([], AccTy, AccCs, _NoAny, _N, _Env) ->
    1250	12	    {AccTy, AccCs}.

--- test/should_pass/tuple_union_pass.erl
+++ test/should_pass/tuple_union_pass.erl
@@ -1,7 +1,14 @@
 -module(tuple_union_pass).
 
--export([f/0]).
+-export([f/0,
+         g/0]).
 
 -spec f() -> {integer()} | {boolean()}.
 f() ->
+    {true}.
+
+-type t() :: {float()} | {binary()}.
+
+-spec g() -> t() | {integer()} | {boolean()}.
+g() ->
     {true}.
