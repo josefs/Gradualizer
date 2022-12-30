8a5722ac6f852ca277f4aa40b23fd5e9fb2c56d6
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-12-30 13:07:57 +0100

    Add test to test/known_problems/should_pass/poly_should_pass.erl
    Based on:
    
        -spec get_unassigned_fields(Fields, All) -> [atom()] when
              Fields :: [record_field() | typed_record_field()],
              All :: [typed_record_field()].
        get_unassigned_fields(Fields, All) ->
            FieldNames = lists:flatmap(fun
                                           (?record_field(Field)) -> [Field];
                                           (?typed_record_field(Field)) -> [Field];
                                           %% We might run into values like this, which don't match above:
                                           %% {record_field, _, {var,{12,11},'_'}, _}
                                           (_) -> []
                                       end, Fields),
            AllNames = lists:map(fun (?typed_record_field(Field)) -> Field end, All),
            AllNames -- FieldNames.
    
    Which currently raises:
    
        ebin/typechecker.beam: Lower bound record_field() | typed_record_field()
            of type variable A_typechecker_3467_838 on line 2026 is not a subtype
            of {A_typechecker_3467_838_typechecker_1226_848,
                A_typechecker_3467_838_typechecker_1226_849,
                A_typechecker_3467_838_typechecker_1226_850}

--- test/known_problems/should_pass/poly_should_pass.erl
+++ test/known_problems/should_pass/poly_should_pass.erl
@@ -2,7 +2,8 @@
 
 -gradualizer([solve_constraints]).
 
--export([find1/0]).
+-export([find1/0,
+         f/1]).
 
 -spec lookup(T1, [{T1, T2}]) -> (none | T2).
 lookup(_, []) -> none;
@@ -15,3 +16,14 @@ find1() ->
         none -> "default";
         V -> V
     end.
+
+-type t1() :: {_, _, _, _}.
+-type t2() :: {_, _, _}.
+
+-spec f([t1() | t2()]) -> ok.
+f(Fields) ->
+    lists:flatmap(fun
+                      ({_, _, _, _}) -> [t1];
+                      ({_, _, _}) -> [t2]
+                  end, Fields),
+    ok.
