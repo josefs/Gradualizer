a84487de660d4901f007bb995577a2b3c9b2be82
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-12-30 11:23:15 +0100

    Move test from test/should_pass/poly_union_lower_bound_pass.erl to test/known_problems/should_pass/poly_lists_map_constraints_should_pass.erl

--- test/known_problems/should_pass/poly_lists_map_constraints_should_pass.erl
+++ test/known_problems/should_pass/poly_lists_map_constraints_should_pass.erl
@@ -4,8 +4,9 @@
 
 %% This is a minimised problem from type checking `gradualizer_db:collect_specs/1'
 %% with constraint solving enabled.
-
 -export([map_many/1]).
+
+-export([j/1]).
 
 -spec map_many({_, nonempty_list()}) -> nonempty_list().
 map_many({_Key, Types}) ->
@@ -23,3 +24,12 @@ map_elem(b) -> a.
 -spec map_specific_list(t_list()) -> t_list().
 map_specific_list(List) ->
     lists:map(fun (E) -> E end, List).
+
+-spec j([binary() | integer()]) -> list().
+j(L) ->
+    lists:map(fun takes_an_intersection/1, L).
+
+-spec takes_an_intersection(binary()) -> binary();
+                           (integer()) -> integer().
+takes_an_intersection(B) when is_binary(B) -> B;
+takes_an_intersection(I) when is_integer(I) -> I.
--- test/should_pass/poly_union_lower_bound_pass.erl
+++ test/should_pass/poly_union_lower_bound_pass.erl
@@ -2,8 +2,7 @@
 
 -gradualizer([solve_constraints]).
 
--export([i/1,
-         j/1]).
+-export([i/1]).
 
 -spec i([binary() | integer()]) -> [integer()].
 i(L) ->
@@ -12,12 +11,3 @@ i(L) ->
 -spec takes_a_union(binary() | integer()) -> integer().
 takes_a_union(B) when is_binary(B) -> binary_to_integer(B);
 takes_a_union(I) when is_integer(I) -> I.
-
--spec j([binary() | integer()]) -> list().
-j(L) ->
-    lists:map(fun takes_an_intersection/1, L).
-
--spec takes_an_intersection(binary()) -> binary();
-                           (integer()) -> integer().
-takes_an_intersection(B) when is_binary(B) -> B;
-takes_an_intersection(I) when is_integer(I) -> I.
