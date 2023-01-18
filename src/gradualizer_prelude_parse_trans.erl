9a5bd852f060f706f42d1e01a14a0a18bc48af2e
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2023-01-18 20:16:33 +0100

    Fix type error in generated gradualizer_prelude forms
    This fixes:
    
        ebin/gradualizer_prelude.beam: The tuple on line 0 is expected to have type erl_parse:abstract_form() | erl_parse:form_info() but it has type {attribute, 1, file, {[$...$v, ...], 1}}
    
        get_modules_and_forms() ->
            [{erlang,
              [{attribute,
               ^^^^^^^^^^^
                1,
                ^^
                file,
                ^^^^^
                {"src/../priv/prelude/erlang.specs.erl", 1}},
                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    This error stems from `erl_anno:anno()` being an opaque type.
    It means that literal 1 is not treated as an `erl_anno:anno()` instance,
    as remote opaque types cannot by compared by structural comparison.
    Instead, we have to create opaque type instances explicitly by calling constructors,
    i.e. functions that return their instances.
    
    That's what we're doing in this commit.

--- src/gradualizer_prelude_parse_trans.erl
+++ src/gradualizer_prelude_parse_trans.erl
@@ -6,7 +6,8 @@
 
 -export([parse_transform/2, get_module_forms_tuples/1]).
 
--type forms() :: [erl_parse:abstract_form() | {error, _} | {eof, _}].
+-type form() :: erl_parse:abstract_form() | {error, _} | {eof, _}.
+-type forms() :: [form()].
 
 -include("gradualizer.hrl").
 
@@ -18,7 +19,7 @@ replace_get_modules_and_forms([{function, Anno, get_modules_and_forms, 0, _OldBo
     PreludeDir = filename:join([filename:dirname(?FILE), "..", "priv", "prelude"]),
     ModuleFormsTuples = get_module_forms_tuples(PreludeDir),
     BodyClauses = [{clause, Anno, [], [],
-                    [erl_parse:abstract(ModuleFormsTuples)]}],
+                    [erl_anno_replace(erl_parse:abstract(ModuleFormsTuples))]}],
     [{function, Anno, get_modules_and_forms, 0, BodyClauses} | RestForms];
 replace_get_modules_and_forms([Form | RestForms]) ->
     [Form | replace_get_modules_and_forms(RestForms)].
@@ -35,4 +36,21 @@ get_module_and_forms(File) ->
     {ok, Forms} = epp:parse_file(File, []),
     [{attribute, _, file, _},
      {attribute, _, module, Module} | _] = Forms,
-    {Module, Forms}.
+    {Module, [ erl_anno_mark(F) || F <- Forms ]}.
+
+erl_anno_mark({eof, _} = Form) ->
+    Form;
+erl_anno_mark(Form) ->
+    Anno = element(2, Form),
+    setelement(2, Form, {'$anno', Anno}).
+
+erl_anno_replace(AST) ->
+    erl_syntax:revert(erl_syntax_lib:map(fun replace_mark/1, AST)).
+
+replace_mark(T) ->
+    case T of
+        {tree, tuple, _, [{atom, _, '$anno'}, Anno]} ->
+            erl_syntax:application(erl_syntax:atom("erl_anno"), erl_syntax:atom("new"), [Anno]);
+        _ ->
+            T
+    end.
