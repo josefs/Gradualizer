5aa2a1b2cb0f58c54e82deebd9dcf92df928539f
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2023-01-26 22:24:03 +0100

    Rearrange tests for list comprehension generators being none()

--- test/should_pass/lc_generator_not_none.erl
+++ test/should_pass/lc_generator_not_none.erl
@@ -1,10 +1,9 @@
 -module(lc_generator_not_none).
 
-%% Compare with:
-%% - test/known_problems/should_pass/lc_cannot_glb_different_variants.erl
-%% - test/should_fail/lc_generator_should_fail.erl
+%% Compare with test/should_fail/lc_generator_not_none_fail.erl
 
--export([g/1]).
+-export([g/1,
+         h/1]).
 
 -type t() :: {a, d}
            | {a, [t()]}.
@@ -13,4 +12,8 @@
 g({a, d} = T) ->
     [T];
 g({a, Ts}) ->
+    [ T || T <- Ts ].
+
+-spec h(t()) -> [t()].
+h({a, Ts}) ->
     [ T || T <- Ts ].
