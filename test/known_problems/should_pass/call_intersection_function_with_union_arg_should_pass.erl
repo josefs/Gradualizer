a4962f039b684cf8eb85340e33b911f19077fa86
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2023-02-01 21:19:36 +0100

    Extend test/known_problems/should_pass/call_intersection_function_with_union_arg_should_pass.erl

--- test/known_problems/should_pass/call_intersection_function_with_union_arg_should_pass.erl
+++ test/known_problems/should_pass/call_intersection_function_with_union_arg_should_pass.erl
@@ -1,14 +1,25 @@
 -module(call_intersection_function_with_union_arg_should_pass).
 
--export([f/1]).
+-export([f/1,
+         g/2]).
 
 -type t() :: t1 | t2.
+-type u() :: u1 | u2.
 
 -spec f(t()) -> ok.
 f(T) ->
-    helper(T).
+    f_(T).
 
--spec helper(t1) -> ok;
-            (t2) -> ok.
-helper(t1) -> ok;
-helper(t2) -> ok.
+-spec f_(t1) -> ok;
+        (t2) -> ok.
+f_(t1) -> ok;
+f_(t2) -> ok.
+
+-spec g(t(), u()) -> ok.
+g(T, U) ->
+    g_(T, U).
+
+-spec g_(t1, u1) -> ok;
+        (t2, u2) -> ok.
+g_(t1, u1) -> ok;
+g_(t2, u2) -> ok.
