9f3c1e6b783bed151cbd5a1196f8c0d166a9a2f8
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2023-02-15 20:03:04 +0100

    Drop case already tested as call_intersection_function_with_union_arg_pass:f2/1

--- test/known_problems/should_pass/call_intersection_function_with_union_arg_should_pass.erl
+++ test/known_problems/should_pass/call_intersection_function_with_union_arg_should_pass.erl
@@ -1,19 +1,9 @@
 -module(call_intersection_function_with_union_arg_should_pass).
 
--export([f/1,
-         g/2]).
+-export([g/2]).
 
 -type t() :: t1 | t2.
 -type u() :: u1 | u2.
-
--spec f(t()) -> ok.
-f(T) ->
-    f_(T).
-
--spec f_(t1) -> ok;
-        (t2) -> ok.
-f_(t1) -> ok;
-f_(t2) -> ok.
 
 -spec g(t(), u()) -> ok.
 g(T, U) ->
