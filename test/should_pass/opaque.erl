ac0ed52232c4f449eb33082e64edfe03f8534b7d
David Pavlík <david@dpavlik.cz>
2024-02-08 15:22:13 +0100

    Add additional tests for opaque types

--- test/should_pass/opaque.erl
+++ test/should_pass/opaque.erl
@@ -1,11 +1,20 @@
 -module(opaque).
 
--export([external/2, internal/2, use_internal/2]).
+-export([external/2, external_update/0, internal/2, use_internal/2, external_return/0]).
 -export_type([my_opaque/0]).
 
 -spec external(user_types:my_opaque(), integer() | undefined) -> integer().
 external(_, undefined) -> 0;
 external(_, I) -> I.
+
+-spec external_update() -> ok.
+external_update() ->
+    Val = user_types:new_opaque(),
+    _Val2 = user_types:update_opaque(Val),
+    ok.
+
+-spec external_return() -> user_types:my_opaque().
+external_return() -> user_types:new_opaque().
 
 -opaque my_opaque() :: integer().
 
