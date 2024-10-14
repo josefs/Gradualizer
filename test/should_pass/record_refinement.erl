f097860949ec20e1994fa9e313fd9b636dfffbc7
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2024-10-14 22:22:12 +0200

    Fix test/should_pass/record_refinement.erl

--- test/should_pass/record_refinement.erl
+++ test/should_pass/record_refinement.erl
@@ -1,6 +1,14 @@
 -module(record_refinement).
 
--compile([export_all, nowarn_export_all]).
+-export([one_field/2,
+         one_field2/2,
+         two_field/2,
+         multiple/1,
+         underscore/1,
+         type_var/1,
+         with_spec/1,
+         two_level1/1,
+         two_level2/1]).
 
 -record(one_field, {a :: integer() | undefined}).
 
@@ -48,39 +56,21 @@ without_spec(R) -> with_spec(R#any.f).
 -spec with_spec(integer()) -> integer().
 with_spec(I) -> I + 1.
 
--record(refined_field, {f :: integer() | undefined}).
--spec refined_field(#refined_field{}) -> #refined_field{f :: integer()}.
-refined_field(#refined_field{f = undefined}) -> #refined_field{f = 0};
-refined_field(R) -> R.
-
--spec refined_field_safe(#refined_field{f :: integer()}) -> #refined_field{f :: integer()}.
-refined_field_safe(#refined_field{f = I}) -> #refined_field{f = I + 1}.
-
--spec refined_field_unsafe(#refined_field{}) -> #refined_field{}.
-refined_field_unsafe(R = #refined_field{f = undefined}) -> R;
-refined_field_unsafe(R) -> refined_field_safe(R).
-
 -record(two_level2, {value :: undefined | integer()}).
 -record(two_level1, {two_level2 :: undefined | #two_level2{}}).
 
 -spec two_level1(#two_level1{}) -> integer().
-two_level1(#two_level1{two_level2 = undefined}) -> 
+two_level1(#two_level1{two_level2 = undefined}) ->
     0;
-two_level1(#two_level1{two_level2 = #two_level2{value = undefined}}) -> 
+two_level1(#two_level1{two_level2 = #two_level2{value = undefined}}) ->
     0;
 two_level1(#two_level1{two_level2 = #two_level2{value = Value}}) ->
     Value.
 
 -spec two_level2(#two_level1{}) -> integer().
-two_level2(#two_level1{two_level2 = undefined}) -> 
+two_level2(#two_level1{two_level2 = undefined}) ->
     0;
-two_level2(#two_level1{two_level2 = #two_level2{value = undefined}}) -> 
+two_level2(#two_level1{two_level2 = #two_level2{value = undefined}}) ->
     0;
 two_level2(R1) ->
     R1#two_level1.two_level2#two_level2.value.
-
--record(not_all_fields_refined_r, {a, b}).
--type not_all_fields_refined_t() :: #not_all_fields_refined_r{b :: b | c}.
-
--spec not_all_fields_refined() -> not_all_fields_refined_t().
-not_all_fields_refined() -> #not_all_fields_refined_r{b = c}.
